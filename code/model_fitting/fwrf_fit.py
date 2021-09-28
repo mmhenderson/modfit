changing this code to use only the heldout data for stacking errors, also fixing a bug w r2 computation

code/model_fitting/fwrf_fit.py
@@ -59,7 +59,7 @@ def _loss_fn(_cofactor, _vtrn, _xout, _vout):
     _beta = torch.tensordot(_cofactor, _vtrn, dims=[[2], [0]]) # [#lambdas, #feature, #voxel]
     _pred = torch.tensordot(_xout, _beta, dims=[[1],[1]]) # [#samples, #lambdas, #voxels]
     _loss = torch.sum(torch.pow(_vout[:,None,:] - _pred, 2), dim=0) # [#lambdas, #voxels]
-    return _beta, _loss
+    return _beta, _loss, _pred
 
 
 
@@ -123,11 +123,13 @@ def fit_fwrf_model(images, voxel_data, _feature_extractor, prf_models, lambdas, 
         np.random.shuffle(order)
         
     images = images[order]
-    voxel_data_shuff = copy.deepcopy(voxel_data)
-    voxel_data_shuff = voxel_data_shuff[order]  
-    trn_data = voxel_data_shuff[:trn_size]
-    out_data = voxel_data_shuff[trn_size:]
+    
+    train_trial_order = order[:trn_size]
+    holdout_trial_order = order[trn_size:]
 
+    trn_data = copy.deepcopy(voxel_data[train_trial_order,:])
+    out_data = copy.deepcopy(voxel_data[holdout_trial_order,:])
+    
     
     # Here is where any model-specific additional initialization steps are done
     # Includes initializing pca params arrays, if doing pca
@@ -154,8 +156,8 @@ def fit_fwrf_model(images, voxel_data, _feature_extractor, prf_models, lambdas, 
     best_losses = np.full(fill_value=np.inf, shape=(n_voxels,n_partial_versions), dtype=dtype)
 
     # Initialize arrays to store the trial-wise predictions (need these for stacking)
-    # Note that this is all training set trials - including the held out trials.
-    best_train_preds = np.zeros(shape=(n_voxels, n_trials, n_partial_versions), dtype=dtype)
+    # Using JUST the held out trials here so the errors are always cross-validated
+    best_train_holdout_preds = np.zeros(shape=(n_voxels, holdout_size, n_partial_versions), dtype=dtype)
 
     # Additional params that are optional
     if add_bias:
@@ -245,16 +247,21 @@ def fit_fwrf_model(images, voxel_data, _feature_extractor, prf_models, lambdas, 
                     _vout = torch_utils._to_torch(out_data[:,rv], device=device)
 
                     # Here is where optimization happens - relatively simple matrix math inside loss fn.
-                    _betas, _loss = _loss_fn(_cof, _vtrn, _xout, _vout) #   [#lambda, #feature, #voxel, ], [#lambda, #voxel]
+                    _betas, _loss, _pred_out = _loss_fn(_cof, _vtrn, _xout, _vout) #   [#lambda, #feature, #voxel, ], [#lambda, #voxel], [trials x lambdas x voxels]
+                    # Keep trial-by-trial predictions for each held-out set trial (need for stacking)
+                    pred_out = torch_utils.get_value(_pred_out) 
+                    # Note these trials are still in their SHUFFLED order, so we when we want to get R2 
+                    # will need to make the actual data match this order.
+            
                     
-                    # Get trial-by-trial predictions for each training set trial (need for stacking)
-                    _pred_train = torch.tensordot(_xtrn, _betas, dims=[[1],[1]]) # [#samples, #lambdas, #voxels]
-                    _pred_out = torch.tensordot(_xout, _betas, dims=[[1],[1]]) # [#samples, #lambdas, #voxels]
-                    pred_train = torch_utils.get_value(_pred_train)
-                    pred_out = torch_utils.get_value(_pred_out)
+                    
+#                     _pred_train = torch.tensordot(_xtrn, _betas, dims=[[1],[1]]) # [#samples, #lambdas, #voxels]
+#                     _pred_out = torch.tensordot(_xout, _betas, dims=[[1],[1]]) # [#samples, #lambdas, #voxels]
+#                     pred_train = torch_utils.get_value(_pred_train)
+                    
                     # Going to combine the training and held out trials and re-create their original order here.
-                    preds_all_shuffled = np.concatenate((pred_train, pred_out), axis=0)
-                    preds_all_origorder = numpy_utils.unshuffle(preds_all_shuffled, order) # [#samples x lambdas x voxels]
+#                     preds_all_shuffled = np.concatenate((pred_train, pred_out), axis=0)
+#                     preds_all_origorder = numpy_utils.unshuffle(preds_all_shuffled, order) # [#samples x lambdas x voxels]
     
                     # Now have a set of weights (in betas) and a loss value for every voxel and every lambda. 
                     # goal is then to choose for each voxel, what is the best lambda and what weights went with that lambda.
@@ -310,7 +317,7 @@ def fit_fwrf_model(images, voxel_data, _feature_extractor, prf_models, lambdas, 
                         
                         # Save the trialwise predictions for all trials in their original order.
                         # Choosing predictions from whichever lambda was best.
-                        best_train_preds[arv,:,pp] = numpy_utils.select_along_axis(preds_all_origorder[:,:,imp], \
+                        best_train_holdout_preds[arv,:,pp] = numpy_utils.select_along_axis(pred_out[:,:,imp], \
                                                                                lambda_inds, run_axis=2, choice_axis=1).T;
 
                 vox_loop_time += (time.time() - vox_start)
@@ -337,4 +344,4 @@ def fit_fwrf_model(images, voxel_data, _feature_extractor, prf_models, lambdas, 
     best_params = [prf_models[best_prf_models],]+return_params+[features_mean, features_std]+[best_prf_models]
     sys.stdout.flush()
 
-    return best_losses, best_lambdas, best_params, best_train_preds
+    return best_losses, best_lambdas, best_params, best_train_holdout_preds, holdout_trial_order

code/model_fitting/fwrf_predict.py
@@ -13,6 +13,7 @@ def get_r2(actual,predicted):
     """
     This computes the coefficient of determination (R2).
     Always goes along first dimension (i.e. the trials/samples dimension)
+    MAKE SURE INPUTS ARE ACTUAL AND THEN PREDICTED, NOT FLIPPED
     """
     ssres = np.sum(np.power((predicted - actual),2), axis=0);
     sstot = np.sum(np.power((actual - np.mean(actual, axis=0)),2), axis=0);
@@ -173,11 +174,11 @@ def validate_fwrf_model(best_params, prf_models, voxel_data, images, _feature_ex
     return val_cc, val_r2, pred_voxel_data
 
 
-def run_stacking(_feature_extractor, trn_voxel_data, val_voxel_data, trn_voxel_data_pred, val_voxel_data_pred, debug=False):   
+def run_stacking(_feature_extractor, trn_holdout_voxel_data, val_voxel_data, trn_holdout_voxel_data_pred, val_voxel_data_pred, debug=False):   
     """
     Get data organized to run stacking code (stacked_core)
     """
-    n_voxels = trn_voxel_data.shape[1]
+    n_voxels = trn_holdout_voxel_data.shape[1]
 
     # To get the "features" to use for stacking - i'm using the "partial models" that are defined 
     # by the feature extractor. The first one is the full model, so we don't want to use that - just want 
@@ -201,26 +202,26 @@ def run_stacking(_feature_extractor, trn_voxel_data, val_voxel_data, trn_voxel_d
 
     # Creating a list where each element is predictions for one of the partial models - these will be 
     # the 'features' elements input to stacking code.
-    preds_train = [trn_voxel_data_pred[:,:,pp].T for pp in partial_models_use]
+    preds_train = [trn_holdout_voxel_data_pred[:,:,pp].T for pp in partial_models_use]
     preds_val = [val_voxel_data_pred[:,:,pp] for pp in partial_models_use]
     # Compute trial-wise training errors
     # each element of err is [ntrials x nvoxels]
-    train_err = [trn_voxel_data - trn_voxel_data_pred[:,:,pp].T for pp in partial_models_use]
+    train_err = [trn_holdout_voxel_data - trn_holdout_voxel_data_pred[:,:,pp].T for pp in partial_models_use]
 
     # Also computing the performance of each of the partial versions on training set data.
     # this is sort of a sanity check that things are working, since the performance of the partial models
-    # should roughly predict what the stacking weights will be.
-    train_r2 = np.array([get_r2(trn_voxel_data, trn_voxel_data_pred[:,:,pp].T) \
+    # should roughly (?)  predict what the stacking weights will be.
+    train_r2 = np.array([get_r2(trn_holdout_voxel_data, trn_holdout_voxel_data_pred[:,:,pp].T) \
                          for pp in range(len(partial_version_names))]).T
     train_r2 = np.nan_to_num(train_r2)
-    train_cc = np.array([get_corrcoef(trn_voxel_data, trn_voxel_data_pred[:,:,pp].T) \
+    train_cc = np.array([get_corrcoef(trn_holdout_voxel_data, trn_holdout_voxel_data_pred[:,:,pp].T) \
                          for pp in range(len(partial_version_names))]).T
     train_cc = np.nan_to_num(train_cc)
 
     # First running stacking w all features included
     feat_use = np.arange(0,n_feature_groups)
     # Stack result will be a tuple including the stacking weights, performance.
-    stack_result = stacked_core(feat_use, train_err, train_data=trn_voxel_data,\
+    stack_result = stacked_core(feat_use, train_err, train_data=trn_holdout_voxel_data,\
                      val_data = val_voxel_data, preds_train = preds_train, preds_val = preds_val,\
                      debug=debug);
 
@@ -231,7 +232,7 @@ def run_stacking(_feature_extractor, trn_voxel_data, val_voxel_data, trn_voxel_d
         for leave_one in range(n_feature_groups):
             feat_use_lo = list(copy.deepcopy(feat_use))
             feat_use_lo.remove(leave_one)
-            tmp = stacked_core(feat_use_lo, train_err, train_data=trn_voxel_data,\
+            tmp = stacked_core(feat_use_lo, train_err, train_data=trn_holdout_voxel_data,\
                              val_data = val_voxel_data, preds_train = preds_train, preds_val = preds_val,\
                              debug=debug);
             stack_result_lo[leave_one] = tmp
@@ -317,14 +318,14 @@ def stacked_core(feat_use, train_err, train_data, val_data, preds_train, preds_v
         
     print('Computing performance of stacked models')
     # Compute r2 of the stacked model for training data
-    stacked_r2_train = get_r2(stacked_pred_train, train_data)
-    stacked_cc_train = get_corrcoef(stacked_pred_train, train_data)
+    stacked_r2_train = get_r2(train_data, stacked_pred_train)
+    stacked_cc_train = get_corrcoef(train_data, stacked_pred_train)
     stacked_r2_train = np.nan_to_num(stacked_r2_train)
     stacked_cc_train = np.nan_to_num(stacked_cc_train) 
     
     # And for validation data
-    stacked_r2_val = get_r2(stacked_pred_val, val_data)
-    stacked_cc_val = get_corrcoef(stacked_pred_val, val_data)
+    stacked_r2_val = get_r2(val_data, stacked_pred_val)
+    stacked_cc_val = get_corrcoef(val_data, stacked_pred_val)
     stacked_r2_val = np.nan_to_num(stacked_r2_val)
     stacked_cc_val = np.nan_to_num(stacked_cc_val) 
     
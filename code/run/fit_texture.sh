e70ec74dd269658c5062e22f28401c5398de0534
Margaret Marie Henderson <mmhender@mind-1-23.eth>
2021-10-18 17:32:16 -0400

    updated script to run texture model fitting

---
+++ code/run/fit_texture.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --exclude=mind-1-13
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=4
+#SBATCH --open-mode=append
+#SBATCH --output=./sbatch_output/output-%A-%x-%u.out 
+#SBATCH --time=8-00:00:00
+
+source ~/myenv/bin/activate
+
+CWD=$(pwd)
+cd ../../
+ROOT=$(pwd)
+
+debug=1
+up_to_sess=1
+
+subj=1
+volume_space=1
+ridge=1
+sample_batch_size=100
+voxel_batch_size=100
+zscore_features=1
+zscore_in_groups=1
+
+fitting_type=pyramid_texture
+use_pca_pyr_feats=0
+do_stack=0
+do_roi_recons=0
+do_voxel_recons=0
+
+cd $ROOT/code/model_fitting
+
+python3 fit_model.py --subject $subj --volume_space $volume_space --up_to_sess $up_to_sess --sample_batch_size $sample_batch_size --voxel_batch_size $voxel_batch_size --zscore_features $zscore_features --zscore_in_groups $zscore_in_groups --ridge $ridge --use_pca_st_feats $use_pca_st_feats --use_lda_st_feats $use_lda_st_feats --min_pct_var $min_pct_var --max_pc_to_retain $max_pc_to_retain --debug $debug --fitting_type $fitting_type --do_stack $do_stack --do_roi_recons $do_roi_recons --do_voxel_recons $do_voxel_recons 

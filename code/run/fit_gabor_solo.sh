d863fc9c0c056271942b824629f78a80babff9df
Margaret Marie Henderson <mmhender@mind.cs.cmu.edu>
2021-11-08 12:25:34 -0500

    running fitting

--- code/run/fit_gabor_solo.sh
+++ code/run/fit_gabor_solo.sh
@@ -36,7 +36,8 @@ n_sf=8
 do_stack=0
 do_roi_recons=0
 do_voxel_recons=0
+shuffle_images=1
 
 cd $ROOT/code/model_fitting
 
-python3 fit_model.py --subject $subj --debug $debug --fitting_type $fitting_type --volume_space $volume_space --up_to_sess $up_to_sess --sample_batch_size $sample_batch_size --voxel_batch_size $voxel_batch_size --zscore_features $zscore_features --zscore_in_groups $zscore_in_groups --ridge $ridge --n_ori $n_ori --n_sf $n_sf --do_stack $do_stack --do_roi_recons $do_roi_recons --do_voxel_recons $do_voxel_recons --which_prf_grid $which_prf_grid --do_tuning $do_tuning --do_sem_disc $do_sem_disc
+python3 fit_model.py --subject $subj --debug $debug --fitting_type $fitting_type --volume_space $volume_space --up_to_sess $up_to_sess --sample_batch_size $sample_batch_size --voxel_batch_size $voxel_batch_size --zscore_features $zscore_features --zscore_in_groups $zscore_in_groups --ridge $ridge --n_ori $n_ori --n_sf $n_sf --do_stack $do_stack --do_roi_recons $do_roi_recons --do_voxel_recons $do_voxel_recons --which_prf_grid $which_prf_grid --do_tuning $do_tuning --do_sem_disc $do_sem_disc --shuffle_images $shuffle_images
--- code/run/fit_sketch_tokens.sh
+++ code/run/fit_sketch_tokens.sh
@@ -15,7 +15,7 @@ cd ../../
 ROOT=$(pwd)
 
 debug=0
-up_to_sess=40
+up_to_sess=20
 
 subj=1
 volume_space=1
@@ -31,17 +31,17 @@ use_pca_st_feats=0
 use_lda_st_feats=0
 do_stack=0
 lda_discrim_type=None
+
 do_roi_recons=0
 do_voxel_recons=0
-# which_prf_grid=3
-do_fitting=1
-date_str=0
+
+which_prf_grid=4
+do_fitting=0
+date_str=Nov-04-2021_2314_17
+do_val=1
+do_tuning=1
+do_sem_disc=1
 
 cd $ROOT/code/model_fitting
 
-# date_str=Nov-03-2021_1939_00
-# python3 fit_model.py --subject $subj --volume_space $volume_space --up_to_sess $up_to_sess --sample_batch_size $sample_batch_size --voxel_batch_size $voxel_batch_size --zscore_features $zscore_features --zscore_in_groups $zscore_in_groups --ridge $ridge --use_pca_st_feats $use_pca_st_feats --use_lda_st_feats $use_lda_st_feats --debug $debug --fitting_type $fitting_type --do_stack $do_stack --do_roi_recons $do_roi_recons --do_voxel_recons $do_voxel_recons --lda_discrim_type $lda_discrim_type --use_precomputed_prfs $use_precomputed_prfs --which_prf_grid $which_prf_grid --do_fitting $do_fitting --date_str $date_str
-
-# date_str=Nov-04-2021_0115_51
-which_prf_grid=4
-python3 fit_model.py --subject $subj --volume_space $volume_space --up_to_sess $up_to_sess --sample_batch_size $sample_batch_size --voxel_batch_size $voxel_batch_size --zscore_features $zscore_features --zscore_in_groups $zscore_in_groups --ridge $ridge --use_pca_st_feats $use_pca_st_feats --use_lda_st_feats $use_lda_st_feats --debug $debug --fitting_type $fitting_type --do_stack $do_stack --do_roi_recons $do_roi_recons --do_voxel_recons $do_voxel_recons --lda_discrim_type $lda_discrim_type --use_precomputed_prfs $use_precomputed_prfs --which_prf_grid $which_prf_grid --do_fitting $do_fitting --date_str $date_str
+python3 fit_model.py --subject $subj --volume_space $volume_space --up_to_sess $up_to_sess --sample_batch_size $sample_batch_size --voxel_batch_size $voxel_batch_size --zscore_features $zscore_features --zscore_in_groups $zscore_in_groups --ridge $ridge --use_pca_st_feats $use_pca_st_feats --use_lda_st_feats $use_lda_st_feats --debug $debug --fitting_type $fitting_type --do_stack $do_stack --do_roi_recons $do_roi_recons --do_voxel_recons $do_voxel_recons --lda_discrim_type $lda_discrim_type --use_precomputed_prfs $use_precomputed_prfs --which_prf_grid $which_prf_grid --do_fitting $do_fitting --do_val $do_val --date_str $date_str --do_tuning $do_tuning --do_sem_disc $do_sem_disc

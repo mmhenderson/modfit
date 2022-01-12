5f1eaa8f5f1522285b9e71fb6500c53d8072f2e7
Margaret Marie Henderson <mmhender@cmu.edu>
2022-01-11 20:20:42 -0500

    adding options for all feature sets

--- code/feature_extraction/linear_discr.py
+++ code/feature_extraction/linear_discr.py
@@ -3,54 +3,99 @@ import numpy as np
 import time, h5py
 codepath = '/user_data/mmhender/imStat/code'
 sys.path.append(codepath)
-from utils import default_paths, coco_utils, nsd_utils, numpy_utils, stats_utils
+from utils import default_paths, nsd_utils, numpy_utils, stats_utils
 from model_fitting import initialize_fitting 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import argparse
 import pandas as pd   
     
-def find_lda_axes(subject, discrim_type='animacy', which_prf_grid=1, debug=False):
+def find_lda_axes(subject, feature_type, discrim_type='animacy', which_prf_grid=1, debug=False):
 
-    # load full-dimension features, already computed 
-    path_to_load = default_paths.sketch_token_feat_path
-    features_file = os.path.join(path_to_load, 'S%d_features_each_prf_grid%d.h5py'%(subject, \
-                                                                                    which_prf_grid))
+    print('\nusing prf grid %d\n'%(which_prf_grid))
+    # Params for the spatial aspect of the model (possible pRFs)
+    models = initialize_fitting.get_prf_models(which_grid = which_prf_grid)    
+    
     labels_folder = os.path.join(default_paths.stim_labels_root, 'S%d_within_prf_grid%d'%(subject, \
-                                                                                      which_prf_grid))   
-    if not os.path.exists(features_file):
-        raise RuntimeError('Looking at %s for precomputed features, not found.'%features_file)   
-    print('Loading pre-computed features from %s'%features_file)
-    t = time.time()
-    with h5py.File(features_file, 'r') as data_set:
-        values = np.copy(data_set['/features'])
-        data_set.close() 
-    elapsed = time.time() - t
-    print('Took %.5f seconds to load file'%elapsed)
-    features_each_prf = values
-    n_features = 150
-    features_each_prf = features_each_prf[:,0:n_features,:]
-
-    # Choose where to save results
-    path_to_save = os.path.join(path_to_load, 'LDA')
-    if not os.path.exists(path_to_save):
-        os.mkdir(path_to_save)
-
-    # how to do z-scoring? can set up groups of columns here.
-    zgroup_labels = np.concatenate([np.zeros(shape=(1,150)), np.ones(shape=(1,1))], axis=1)
-    zgroup_labels = zgroup_labels[0,0:n_features]
+                                                                                      which_prf_grid))  
     
     # training / validation data always split the same way - shared 1000 inds are validation.
     subject_df = nsd_utils.get_subj_df(subject)
     valinds = np.array(subject_df['shared1000'])
     trninds = np.array(subject_df['shared1000']==False)
     
-    print('Size of features array for this image set is:')
-    print(features_each_prf.shape)
+    if feature_type=='sketch_tokens':
 
-    # Params for the spatial aspect of the model (possible pRFs)
-    models = initialize_fitting.get_prf_models(which_grid=which_prf_grid)    
+        path_to_load = default_paths.sketch_token_feat_path
+        features_file = os.path.join(path_to_load, 'S%d_features_each_prf_grid%d.h5py'%(subject, \
+                                                                                        which_prf_grid)) 
+         # how to do z-scoring? can set up groups of columns here.
+        zgroup_labels = np.concatenate([np.zeros(shape=(150,)), np.ones(shape=(1,))], axis=0)
+        features_to_use = np.ones(np.shape(zgroup_labels))==1
+        n_features = 151
+        
+    elif (feature_type=='pyramid_texture_ll') or (feature_type=='pyramid_texture_hl'):
+        
+        path_to_load = default_paths.pyramid_texture_feat_path      
+        n_ori = 4; n_sf = 4;
+        features_file = os.path.join(path_to_load, 'S%d_features_each_prf_%dori_%dsf_grid%d.h5py'%\
+                                     (subject,n_ori, n_sf, which_prf_grid))  
+        # Get dims of each feature type
+        feature_type_dims_ll = [ 6, 16, 16, 10,  1];
+        n_ll_feats = np.sum(feature_type_dims_ll)
+        feature_type_dims_hl = [272,  73,  25,  24,  24,  48,  96,  10,  20];  
+        n_hl_feats = np.sum(feature_type_dims_hl)
+        if feature_type=='pyramid_texture_ll':   
+            # Define groups of columns to zscore within.
+            # Treating every pixel statistic as a different group because of different scales.
+            # Keeping the groups of mean magnitude features together across orients and scales - rather
+            # than z-scoring each column, to preserve informative difference across these channels.
+            zgroup_sizes_ll = [1,1,1,1,1,1] + list(feature_type_dims_ll[1:])
+            zgroup_labels_ll = np.concatenate([np.ones(shape=(1, zgroup_sizes_ll[ff]))*ff \
+                                               for ff in range(len(zgroup_sizes_ll))], axis=1)
+            # For the marginal stats of lowpass recons, separating skew/kurtosis here
+            zgroup_labels_ll[zgroup_labels_ll==9] = 10
+            zgroup_labels_ll[0,np.where(zgroup_labels_ll==8)[1][np.arange(1,10,2)]] = 9
+            zgroup_labels = zgroup_labels_ll
+            features_to_use = np.arange(n_ll_feats+n_hl_feats)<n_ll_feats
+            n_features = n_ll_feats
+        else:
+            zgroup_sizes_hl = list(feature_type_dims_hl)
+            # for higher level groups, just retaining original grouping scheme 
+            zgroup_labels_hl = np.concatenate([np.ones(shape=(1, zgroup_sizes_hl[ff]))*ff \
+                                                   for ff in range(len(zgroup_sizes_hl))], axis=1)
+            zgroup_labels = zgroup_labels_hl
+            features_to_use = np.arange(n_ll_feats+n_hl_feats)>=n_ll_feats
+            n_features = n_hl_feats
+            
+    elif feature_type=='gabor_solo':
+        
+        path_to_load = default_paths.gabor_texture_feat_path   
+        n_ori = 12; n_sf = 8;
+        features_file = os.path.join(path_to_load, \
+                                 'S%d_features_each_prf_%dori_%dsf_gabor_solo_nonlin_grid%d.h5py'%\
+                                 (subject,n_ori, n_sf, which_prf_grid))
+        zgroup_labels = np.ones((n_ori*n_sf,))
+        features_to_use = np.ones(np.shape(zgroup_labels))==1
+        n_features = n_ori*n_sf
+        
+    else:
+        raise RuntimeError('feature type %s not recognized'%feature_type)
+      
+    if not os.path.exists(features_file):
+        raise RuntimeError('Looking at %s for precomputed features, not found.'%features_file)   
+
+    # Choose where to save results
+    path_to_save = os.path.join(path_to_load, 'LDA')
+    if not os.path.exists(path_to_save):
+        os.mkdir(path_to_save)
+    fn2save = os.path.join(path_to_save, 'S%d_%s_LDA_%s_grid%d.npy'%(subject, feature_type, discrim_type, which_prf_grid))
+                              
+    prf_batch_size = 50 # batching prfs for loading, because it is a bit faster
     n_prfs = models.shape[0]
-    
+    n_prf_batches = int(np.ceil(n_prfs/prf_batch_size))          
+    prf_batch_inds = [np.arange(prf_batch_size*bb, np.min([prf_batch_size*(bb+1), n_prfs])) for bb in range(n_prf_batches)]
+    prf_inds_loaded = []
+ 
     scores_each_prf = []
     wts_each_prf = []
     pre_mean_each_prf = []
@@ -67,12 +112,15 @@ def find_lda_axes(subject, discrim_type='animacy', which_prf_grid=1, debug=False
         print('Reading labels from %s...'%coco_labels_fn)
         coco_df = pd.read_csv(coco_labels_fn, index_col=0)
         ims_to_use = np.sum(np.array(coco_df)==1, axis=1)==1
-        print('Proportion with clear indoor/outdoor label (whole image):')
-        print(np.mean(ims_to_use))
-        labels = np.array(coco_df['has_indoor']).astype('int')        
-        print('Of these, proportion indoor:')
-        print(np.mean(labels[ims_to_use]==1))
-        unvals = np.unique(labels[ims_to_use])        
+        labels = np.array(coco_df['has_indoor']).astype(np.float32)
+        labels[~ims_to_use] = np.nan
+        neach = [np.sum(labels==ll) for ll in np.unique(labels[~np.isnan(labels)])] + \
+                [np.sum(np.isnan(labels))]
+        print('n outdoor/n indoor/n ambiguous:')
+        print(neach)
+        unvals = np.unique(labels[ims_to_use])                               
+        print('unique values:')
+        print(unvals)       
    
     for prf_model_index in range(n_prfs):
 
@@ -80,67 +128,82 @@ def find_lda_axes(subject, discrim_type='animacy', which_prf_grid=1, debug=False
             continue
             
         print('\nProcessing pRF %d of %d'%(prf_model_index, n_prfs))
+        if prf_model_index not in prf_inds_loaded:
 
-        # Gather semantic labels for the images, specific to this pRF position. 
-        if discrim_type=='animacy':
-            coco_labels_fn = os.path.join(labels_folder, \
-                                          'S%d_cocolabs_binary_prf%d.csv'%(subject, prf_model_index))
-            print('Reading labels from %s...'%coco_labels_fn)
-            coco_df = pd.read_csv(coco_labels_fn, index_col=0)
-            labels = np.array(coco_df['has_animate'])
-            unvals = np.unique(labels)
-            print('Overall proportion animate this pRF:')
-            print(np.mean(labels==1))
-            print('Proportion w any annotation this pRF:')
-            mat = np.array(coco_df)
-            ims_to_use = np.any(mat, axis=1)
-            print(np.mean(ims_to_use))
-            ims_to_use = np.ones(np.shape(labels))==1
+            batch_to_use = np.where([prf_model_index in prf_batch_inds[bb] for \
+                                     bb in range(len(prf_batch_inds))])[0][0]
+            assert(prf_model_index in prf_batch_inds[batch_to_use])
+
+            print('Loading pre-computed features for prf models [%d - %d] from %s'%\
+                  (prf_batch_inds[batch_to_use][0],prf_batch_inds[batch_to_use][-1], features_file))
+            features_each_prf_batch = None
+
+            t = time.time()
+            with h5py.File(features_file, 'r') as data_set:
+                values = np.copy(data_set['/features'][:,:,prf_batch_inds[batch_to_use]])
+                data_set.close() 
+            elapsed = time.time() - t
+            print('Took %.5f seconds to load file'%elapsed)
+
+            prf_inds_loaded = prf_batch_inds[batch_to_use]
+            features_each_prf_batch = values[:,features_to_use,:].astype(np.float32)
+            values=None
+                                 
+        index_into_batch = np.where(prf_model_index==prf_inds_loaded)[0][0]
+        print('Index into batch for prf %d: %d'%(prf_model_index, index_into_batch))
+        features_in_prf = features_each_prf_batch[:,:,index_into_batch]
+        assert(features_in_prf.shape[1]==n_features)
+        values=None
+        print('Size of features array for this image set and prf is:')
+        print(features_in_prf.shape)
+
             
-        elif discrim_type=='indoor_outdoor':
+        # Gather semantic labels for the images, specific to this pRF position.
+        if discrim_type=='indoor_outdoor':
             labels = labels
             ims_to_use = ims_to_use
-            
-        elif discrim_type=='all_supcat':
-            coco_labels_fn = os.path.join(labels_folder, \
-                                          'S%d_cocolabs_binary_prf%d.csv'%(subject, prf_model_index))
-            print('Reading labels from %s...'%coco_labels_fn)
-            coco_df = pd.read_csv(coco_labels_fn, index_col=0)
-            
-            supcat_labels = np.array(coco_df)[:,0:12]
-            
-            labels = [np.where(supcat_labels[ii,:])[0] for ii in range(supcat_labels.shape[0])]
-            labels = np.array([ll[0] if len(ll)>0 else -1 for ll in labels])
-
-            ims_to_use = np.sum(supcat_labels, axis=1)==1
-            print('Proportion of images that have exactly one super-cat label:')
-            print(np.mean(ims_to_use))
-            
-            print('Unique labels:')
-            unvals = np.unique(labels[ims_to_use])
-            print(np.unique(labels[ims_to_use]))
-            
-        elif discrim_type=='person' or discrim_type=='food' or discrim_type=='vehicle' or discrim_type=='animal':
+        elif discrim_type=='animacy' or discrim_type=='person' or discrim_type=='food' \
+                                 or discrim_type=='vehicle' or discrim_type=='animal':
             coco_labels_fn = os.path.join(labels_folder, \
                                           'S%d_cocolabs_binary_prf%d.csv'%(subject, prf_model_index))
             print('Reading labels from %s...'%coco_labels_fn)
             coco_df = pd.read_csv(coco_labels_fn, index_col=0)
             print('Using %s as label'%discrim_type)
-            labels = np.array(coco_df[discrim_type]).astype('int')
-            unvals = np.unique(labels)
-            ims_to_use = np.ones(np.shape(labels))==1
+            if discrim_type=='animacy':
+                labels = np.array(coco_df['has_animate']).astype(np.float32)
+            else:
+                labels = np.array(coco_df[discrim_type]).astype(np.float32)
+            ims_to_use = np.any(np.array(coco_df)[:,0:12]==1, axis=1)
+            labels[~ims_to_use] = np.nan
+            neach = [np.sum(labels==ll) for ll in np.unique(labels[~np.isnan(labels)])] + \
+                    [np.sum(np.isnan(labels))]
+            print('yes %s/no %s/no labels:'%(discrim_type, discrim_type))
+            print(neach)
+            unvals = np.unique(labels[ims_to_use])                               
+            print('unique values:')
+            print(unvals)       
+        elif discrim_type=='all_supcat':
+            coco_labels_fn = os.path.join(labels_folder, \
+                                          'S%d_cocolabs_binary_prf%d.csv'%(subject, prf_model_index))
+            print('Reading labels from %s...'%coco_labels_fn)
+            coco_df = pd.read_csv(coco_labels_fn, index_col=0)            
+            supcat_labels = np.array(coco_df)[:,0:12]            
+            labels = [np.where(supcat_labels[ii,:])[0] for ii in range(supcat_labels.shape[0])]
+            labels = np.array([ll[0] if len(ll)>0 else -1 for ll in labels]).astype(np.float32)
+            ims_to_use = np.sum(supcat_labels, axis=1)==1
+            labels[~ims_to_use] = np.nan
+            print('Proportion of images that have exactly one super-cat label:')
+            print(np.mean(ims_to_use))           
             print('Unique labels:')
-            print(unvals)
-            print('Proportion with %s:'%discrim_type)
-            print(np.mean(labels==1))
-            
+            unvals, counts = np.unique(labels[ims_to_use], return_counts=True)
+            print(np.unique(labels[ims_to_use]))
+            print(counts)
         else:
             raise ValueError('discrimination type %s not recognized.'%discrim_type)
         
-        features_in_prf = features_each_prf[:,:,prf_model_index]
-        print('Size of features array for this image set and prf is:')
-        print(features_in_prf.shape)
-        
+       
+        assert(np.all(ims_to_use==~np.isnan(labels)))
+          
         # z-score in advance of computing lda 
         features_in_prf_z = np.zeros_like(features_in_prf)
         features_in_prf_z[trninds,:] = numpy_utils.zscore_in_groups(features_in_prf[trninds,:], zgroup_labels)
@@ -149,7 +212,7 @@ def find_lda_axes(subject, discrim_type='animacy', which_prf_grid=1, debug=False
         # Identify the LDA subspace, based on training data only.
         _, scalings, trn_acc, trn_dprime, clf = do_lda(features_in_prf_z[trninds & ims_to_use,:], \
                                                        labels[trninds & ims_to_use], \
-                                                       verbose=True, balance_downsample=False)
+                                                       verbose=True, balance_downsample=True)
         
         # applying the transformation to all images. Including both train and validation here.
         scores = clf.transform(features_in_prf_z)
@@ -180,8 +243,7 @@ def find_lda_axes(subject, discrim_type='animacy', which_prf_grid=1, debug=False
         
         sys.stdout.flush()
 
-    fn2save = os.path.join(path_to_save, 'S%d_LDA_%s_grid%d.npy'%(subject, discrim_type, which_prf_grid))
-   
+    
     print('saving to %s'%fn2save)
     np.save(fn2save, {'scores': scores_each_prf,'wts': wts_each_prf, \
                       'pre_mean': pre_mean_each_prf, \
@@ -284,10 +346,6 @@ if __name__ == '__main__':
 
     if args.debug:
         print('DEBUG MODE\n')
-        
-    if args.feature_type=='sketch_tokens':
-       
-        find_lda_axes(subject=args.subject, debug=args.debug==1, discrim_type=args.discrim_type, which_prf_grid=args.which_prf_grid)
-       
-    else:
-        raise ValueError('--feature_type %s is not recognized'%args.feature_type)
+     
+    find_lda_axes(subject=args.subject, feature_type=args.feature_type, debug=args.debug==1, discrim_type=args.discrim_type, which_prf_grid=args.which_prf_grid)
+    

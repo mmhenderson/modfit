868a1c3bafd8ada083e8d88692ef65ade357f8a3
Margaret Marie Henderson <mmhender@cmu.edu>
2022-01-20 14:14:33 -0500

    code to test if gpu is working ok

---
+++ code/extra_code/test_node.py
@@ -0,0 +1,23 @@
+""" 
+test if gpu available and working
+"""
+
+import numpy as np
+import torch 
+
+print ('#device:', torch.cuda.device_count())
+print ('device#:', torch.cuda.current_device())
+print ('device name:', torch.cuda.get_device_name(torch.cuda.current_device()))
+
+device = torch.device("cuda:0") #cuda
+torch.backends.cudnn.enabled=True
+
+print ('\ntorch:', torch.__version__)
+print ('cuda: ', torch.version.cuda)
+print ('cudnn:', torch.backends.cudnn.version())
+print ('dtype:', torch.get_default_dtype())
+
+a = np.random.normal(0,1,[100,200,50])
+a = a.to(device)
+
+print('done')

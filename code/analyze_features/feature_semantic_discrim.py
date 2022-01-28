b302f1a2b3b4a341dd88c3535777e0cf52af9e43
Margaret Marie Henderson <mmhender@cmu.edu>
2022-01-27 21:28:13 -0500

    analyze all features at once, load from load_labels_each_prf

--- code/analyze_features/feature_semantic_discrim.py
+++ code/analyze_features/feature_semantic_discrim.py
@@ -1,27 +1,28 @@
 import sys, os
 import numpy as np
 import time, h5py
+import scipy.stats
 codepath = '/user_data/mmhender/imStat/code'
 sys.path.append(codepath)
-from utils import default_paths, nsd_utils
+from utils import default_paths, nsd_utils, coco_utils, stats_utils
 from model_fitting import initialize_fitting 
 import argparse
 import pandas as pd
    
-def get_discrim(subject, feature_type, discrim_type='animacy', which_prf_grid=1, debug=False):
+def get_feature_discrim(subject, feature_type, discrim_type='animacy', which_prf_grid=1, debug=False):
 
     print('\nusing prf grid %d\n'%(which_prf_grid))
     # Params for the spatial aspect of the model (possible pRFs)
     models = initialize_fitting.get_prf_models(which_grid = which_prf_grid)    
 
-    labels_folder = os.path.join(default_paths.stim_labels_root, 'S%d_within_prf_grid%d'%(subject, \
-                                                                                        which_prf_grid))
-    
     # training / validation data always split the same way - shared 1000 inds are validation.
     subject_df = nsd_utils.get_subj_df(subject)
     valinds = np.array(subject_df['shared1000'])
     trninds = np.array(subject_df['shared1000']==False)
-    
+    labels_all, discrim_type_list, unique_labels_each = coco_utils.load_labels_each_prf(subject, which_prf_grid, \
+                                                     image_inds=np.where(trninds)[0], models=models,verbose=False)
+    n_sem_axes = labels_all.shape[1]
+        
     if feature_type=='sketch_tokens':
 
         path_to_load = default_paths.sketch_token_feat_path
@@ -46,41 +47,27 @@ def get_discrim(subject, feature_type, discrim_type='animacy', which_prf_grid=1,
     if not os.path.exists(features_file):
         raise RuntimeError('Looking at %s for precomputed features, not found.'%features_file)   
   
-    path_to_save = os.path.join(path_to_load, 'semantic_discrim')
-    if not os.path.exists(path_to_save):
-        os.mkdir(path_to_save)
-    fn2save_corrs = os.path.join(path_to_save, 'S%d_corrs_%s_grid%d.npy'%(subject, discrim_type, which_prf_grid))
-    fn2save_dprime = os.path.join(path_to_save, 'S%d_dprime_%s_grid%d.npy'%(subject, discrim_type, which_prf_grid))
-    fn2save_mean = os.path.join(path_to_load, 'S%d_mean_grid%d.npy'%(subject, which_prf_grid))
-    fn2save_var = os.path.join(path_to_load, 'S%d_var_grid%d.npy'%(subject, which_prf_grid))
-    fn2save_covar = os.path.join(path_to_load, 'S%d_covar_grid%d.npy'%(subject, which_prf_grid))                            
+                               
     prf_batch_size = 50 # batching prfs for loading, because it is a bit faster
     n_prfs = models.shape[0]
     n_prf_batches = int(np.ceil(n_prfs/prf_batch_size))          
     prf_batch_inds = [np.arange(prf_batch_size*bb, np.min([prf_batch_size*(bb+1), n_prfs])) for bb in range(n_prf_batches)]
     prf_inds_loaded = []
     
-    if discrim_type=='indoor_outdoor':
-        # this property is defined across whole images, so loading labels outside the pRF loop.
-        coco_labels_fn = os.path.join(default_paths.stim_labels_root, 'S%d_indoor_outdoor.csv'%subject)
-        print('Reading labels from %s...'%coco_labels_fn)
-        coco_df = pd.read_csv(coco_labels_fn, index_col=0)
-        ims_to_use = np.sum(np.array(coco_df)==1, axis=1)==1
-        labels = np.array(coco_df['has_indoor']).astype(np.float32)
-        labels[~ims_to_use] = np.nan
-        neach = [np.sum(labels==ll) for ll in np.unique(labels[~np.isnan(labels)])] + \
-                [np.sum(np.isnan(labels))]
-        print('n outdoor/n indoor/n ambiguous:')
-        print(neach)
-        unvals = np.unique(labels[ims_to_use])                               
-        print('unique values:')
-        print(unvals)
-            
+    path_to_save = os.path.join(path_to_load, 'feature_stats')
+    if not os.path.exists(path_to_save):
+        os.mkdir(path_to_save)
+    fn2save_corrs = os.path.join(path_to_save, 'S%d_semantic_corrs_grid%d.npy'%(subject, which_prf_grid))
+    fn2save_discrim = os.path.join(path_to_save, 'S%d_semantic_discrim_fstat_grid%d.npy'%(subject, which_prf_grid))
+    fn2save_mean = os.path.join(path_to_save, 'S%d_mean_grid%d.npy'%(subject, which_prf_grid))
+    fn2save_var = os.path.join(path_to_save, 'S%d_var_grid%d.npy'%(subject, which_prf_grid))
+    fn2save_covar = os.path.join(path_to_save, 'S%d_covar_grid%d.npy'%(subject, which_prf_grid)) 
+    
     with h5py.File(features_file, 'r') as data_set:
         dims = data_set['/features'].shape
     n_trials, n_features, n_prfs = dims
-    all_corrs = np.zeros((n_features, n_prfs), dtype=np.float32)
-    all_dprime =  np.zeros((n_features, n_prfs), dtype=np.float32)
+    all_corrs = np.zeros((n_features, n_prfs, n_sem_axes), dtype=np.float32)
+    all_discrim =  np.zeros((n_features, n_prfs, n_sem_axes), dtype=np.float32)
     all_mean = np.zeros((n_features, n_prfs), dtype=np.float32)
     all_var =  np.zeros((n_features, n_prfs), dtype=np.float32)
     all_covar =  np.zeros((n_features, n_features, n_prfs), dtype=np.float32)
@@ -127,57 +114,52 @@ def get_discrim(subject, feature_type, discrim_type='animacy', which_prf_grid=1,
         
         sys.stdout.flush()
                                  
-        if discrim_type=='indoor_outdoor':
-            labels = labels
-            ims_to_use = ims_to_use
-        elif discrim_type=='animacy' or discrim_type=='person' or discrim_type=='food' \
-                                 or discrim_type=='vehicle' or discrim_type=='animal':
-            coco_labels_fn = os.path.join(labels_folder, \
-                                          'S%d_cocolabs_binary_prf%d.csv'%(subject, prf_model_index))
-            print('Reading labels from %s...'%coco_labels_fn)
-            coco_df = pd.read_csv(coco_labels_fn, index_col=0)
-            print('Using %s as label'%discrim_type)
-            if discrim_type=='animacy':
-                labels = np.array(coco_df['has_animate']).astype(np.float32)
-            else:
-                labels = np.array(coco_df[discrim_type]).astype(np.float32)
-            ims_to_use = np.any(np.array(coco_df)[:,0:12]==1, axis=1)
-            labels[~ims_to_use] = np.nan
-            neach = [np.sum(labels==ll) for ll in np.unique(labels[~np.isnan(labels)])] + \
-                    [np.sum(np.isnan(labels))]
-            print('yes %s/no %s/no labels:'%(discrim_type, discrim_type))
-            print(neach)
-            unvals = np.unique(labels[ims_to_use])                               
-            print('unique values:')
-            print(unvals)          
-        else:
-            raise ValueError('discrimination type %s not recognized.'%discrim_type)
-            
-        labels_trn = labels[trninds]
-        inds2use = ~np.isnan(labels_trn) 
-        inds1 = (labels_trn==0) & inds2use
-        inds2 = (labels_trn==1) & inds2use
+        for aa in range(n_sem_axes):
 
-        # now computing relationship between each feature and the semantic labels
-        if np.any(inds1) and np.any(inds2):
+            labels = labels_all[:,aa,prf_model_index]
+            inds2use = ~np.isnan(labels)          
+            unique_labels_actual = np.unique(labels[inds2use])
             
-            for ff in range(n_features):
-                if np.std(features_in_prf_trn[inds2use,ff])==0:
-                    print('std==0')
-                all_corrs[ff,prf_model_index] = np.corrcoef(features_in_prf_trn[inds2use,ff], \
-                                                            labels_trn[inds2use])[0,1]
-                # (mu1-mu2) / std
-                all_dprime[ff,prf_model_index] = (np.mean(features_in_prf_trn[inds1,ff]) -\
-                                      np.mean(features_in_prf_trn[inds2,ff]))/np.std(features_in_prf_trn[inds2use,ff])          
-        else:
-            print('model %d - at least one label category is missing'%(prf_model_index))
-            all_corrs[ff,prf_model_index] = np.nan
-            all_dprime[ff,prf_model_index] = np.nan
-            
+            if prf_model_index==0:
+                print('processing axis: %s'%discrim_type_list[aa])
+                print('labels: ')
+                print(unique_labels_each[aa])
+               
+            if np.any(~np.isin(unique_labels_each[aa], unique_labels_actual)):
+                print('missing some labels for axis %d'%aa)
+                print('expected labels')
+                print(unique_labels_each[aa])
+                print('actual labels')
+                print(unique_labels_actual)
+                
+            if len(unique_labels_actual)>1:
+                
+                group_inds = [((labels==ll) & inds2use) for ll in unique_labels_actual]
+                
+                for ff in range(n_features):
+                    
+                    groups = [features_in_prf_trn[gi,ff] for gi in group_inds]
+                    fstat = stats_utils.anova_oneway_warn(groups).statistic
+                    all_discrim[ff, prf_model_index, aa] = fstat
+                
+            else:
+
+                print('nans for model %d, axis %d - need >1 levels'\
+                          %(prf_model_index, aa))
+                all_discrim[:, prf_model_index, aa] = np.nan
+             
+            # for the binary categories, also getting a correlation coefficient (sign matters here)
+            if (len(unique_labels_each[aa])==2) and (len(unique_labels_actual)==2):
+                for ff in range(n_features):
+                    all_corrs[ff,prf_model_index,aa] = stats_utils.numpy_corrcoef_warn(\
+                                                        features_in_prf_trn[inds2use,ff],labels[inds2use])[0,1]
+            else:
+                all_corrs[:,prf_model_index,aa] = np.nan
+                
     print('saving to %s\n'%fn2save_corrs)
     np.save(fn2save_corrs, all_corrs)                     
-    print('saving to %s\n'%fn2save_dprime)
-    np.save(fn2save_dprime, all_dprime)     
+    print('saving to %s\n'%fn2save_discrim)
+    np.save(fn2save_discrim, all_discrim)     
     print('saving to %s\n'%fn2save_mean)
     np.save(fn2save_mean, all_mean)                     
     print('saving to %s\n'%fn2save_var)
@@ -194,8 +176,6 @@ if __name__ == '__main__':
                     help="number of the subject, 1-8")
     parser.add_argument("--feature_type", type=str,default='sketch_tokens',
                     help="what kind of features are we using?")
-    parser.add_argument("--discrim_type", type=str,default='animacy',
-                    help="what semantic labels are we using to classify?")
     parser.add_argument("--debug", type=int,default=0,
                     help="want to run a fast test version of this script to debug? 1 for yes, 0 for no")
     parser.add_argument("--which_prf_grid", type=int,default=1,
@@ -206,5 +186,5 @@ if __name__ == '__main__':
     if args.debug:
         print('DEBUG MODE\n')
 
-    get_discrim(subject=args.subject, feature_type=args.feature_type, debug=args.debug==1, discrim_type=args.discrim_type, which_prf_grid=args.which_prf_grid)
+    get_feature_discrim(subject=args.subject, feature_type=args.feature_type, debug=args.debug==1, which_prf_grid=args.which_prf_grid)
    

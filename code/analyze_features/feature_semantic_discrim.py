3cd13f12b38a0126467b65ebd540d5430d1984e8
Margaret Marie Henderson <mmhender@cmu.edu>
2022-02-04 14:32:15 -0500

    update to use feature loaders

--- code/analyze_features/feature_semantic_discrim.py
+++ code/analyze_features/feature_semantic_discrim.py
@@ -5,11 +5,12 @@ import scipy.stats
 codepath = '/user_data/mmhender/imStat/code'
 sys.path.append(codepath)
 from utils import default_paths, nsd_utils, coco_utils, stats_utils
+from feature_extraction import fwrf_features
 from model_fitting import initialize_fitting 
 import argparse
 import pandas as pd
    
-def get_feature_discrim(subject, feature_type, which_prf_grid=1, debug=False):
+def get_feature_discrim(subject, feature_type, which_prf_grid=1, debug=False, layer_name=None):
 
     print('\nusing prf grid %d\n'%(which_prf_grid))
     # Params for the spatial aspect of the model (possible pRFs)
@@ -22,6 +23,7 @@ def get_feature_discrim(subject, feature_type, which_prf_grid=1, debug=False):
     print('Using images/labels for subjects:')
     print(subjects)
     
+    # First gather all semantic labels
     trninds_list = []
     for si, ss in enumerate(subjects):
         # training / validation data always split the same way - shared 1000 inds are validation.
@@ -47,38 +49,58 @@ def get_feature_discrim(subject, feature_type, which_prf_grid=1, debug=False):
     print(unique_labels_each)
     n_sem_axes = labels_all.shape[1]
         
-    if feature_type=='sketch_tokens':
-
-        path_to_load = default_paths.sketch_token_feat_path
-        features_files = [os.path.join(path_to_load, 'S%d_features_each_prf_grid%d.h5py'%(ss, \
-                                                        which_prf_grid)) for ss in subjects] 
+    # make feature loaders to get visual features
+    if feature_type=='gabor_solo':
+        path_to_load = default_paths.gabor_texture_feat_path
+        feat_loaders = [fwrf_features.fwrf_feature_loader(subject=ss,\
+                                                        which_prf_grid=which_prf_grid,\
+                                                        feature_type='gabor_solo', \
+                                                        n_ori=12, n_sf=8, nonlin=True) for ss in subjects]
     elif feature_type=='pyramid_texture':
-        
-        path_to_load = default_paths.pyramid_texture_feat_path      
-        n_ori = 4; n_sf = 4;
-        features_files = [os.path.join(path_to_load, 'S%d_features_each_prf_%dori_%dsf_grid%d.h5py'%\
-                                     (ss, n_ori, n_sf, which_prf_grid)) for ss in subjects]   
-    elif feature_type=='gabor_solo':
-        
-        path_to_load = default_paths.gabor_texture_feat_path   
-        n_ori = 12; n_sf = 8;
-        features_files = [os.path.join(path_to_load, \
-                                 'S%d_features_each_prf_%dori_%dsf_gabor_solo_nonlin_grid%d.h5py'%\
-                                 (ss, n_ori, n_sf, which_prf_grid)) for ss in subjects]
+        path_to_load = default_paths.pyramid_texture_feat_path
+        feat_loaders = [fwrf_features.fwrf_feature_loader(subject=ss,\
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='pyramid_texture',\
+                                                        n_ori=4, n_sf=4,\
+                                                        include_ll=True, include_hl=True,\
+                                                        use_pca_feats_hl = False) for ss in subjects]       
+
+    elif feature_type=='sketch_tokens':
+        path_to_load = default_paths.sketch_token_feat_path
+        feat_loaders = [fwrf_features.fwrf_feature_loader(subject=ss,\
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='sketch_tokens',\
+                                                        use_pca_feats = False) for ss in subjects]
+
+    elif feature_type=='alexnet':
+        assert(len(subjects)==1) # since these features are pca-ed within subject, can't concatenate.
+        path_to_load = default_paths.alexnet_feat_path
+        if layer_name is None or layer_name=='':
+            raise ValueError('need to specify layer_name for alexnet')
+        feat_loaders = [fwrf_features.fwrf_feature_loader(subject=ss,\
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='alexnet',layer_name=layer_name,\
+                                                        use_pca_feats = True, padding_mode = 'reflect') \
+                                                        for ss in subjects]
+
+    elif feature_type=='clip':
+        assert(len(subjects)==1) # since these features are pca-ed within subject, can't concatenate.
+        path_to_load = default_paths.clip_feat_path
+        if layer_name is None or layer_name=='':
+            raise ValueError('need to specify layer_name for clip')
+        feat_loaders = [fwrf_features.fwrf_feature_loader(subject=ss,\
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='clip',layer_name=layer_name,\
+                                                        model_architecture='RN50',use_pca_feats=True) \
+                                                        for ss in subjects]
+
     else:
         raise RuntimeError('feature type %s not recognized'%feature_type)
-      
-    if not os.path.exists(features_files[0]):
-        raise RuntimeError('Looking at %s for precomputed features, not found.'%features_files[0])   
-  
-                               
-    prf_batch_size = 50 # batching prfs for loading, because it is a bit faster
-    n_prfs = models.shape[0]
-    n_prf_batches = int(np.ceil(n_prfs/prf_batch_size))          
-    prf_batch_inds = [np.arange(prf_batch_size*bb, np.min([prf_batch_size*(bb+1), n_prfs])) for bb in range(n_prf_batches)]
-    prf_inds_loaded = []
     
-    path_to_save = os.path.join(path_to_load, 'feature_stats')
+    if debug:
+        path_to_save = os.path.join(path_to_load, 'feature_stats_DEBUG')
+    else:
+        path_to_save = os.path.join(path_to_load, 'feature_stats')
     if not os.path.exists(path_to_save):
         os.mkdir(path_to_save)
     if subject=='all':
@@ -94,9 +116,8 @@ def get_feature_discrim(subject, feature_type, which_prf_grid=1, debug=False):
         fn2save_var = os.path.join(path_to_save, 'S%s_var_grid%d.npy'%(subject, which_prf_grid))
         fn2save_covar = os.path.join(path_to_save, 'S%s_covar_grid%d.npy'%(subject, which_prf_grid)) 
     
-    with h5py.File(features_files[0], 'r') as data_set:
-        dims = data_set['/features'].shape
-    n_trials, n_features, n_prfs = dims
+    n_features = feat_loaders[0].max_features
+    n_prfs = models.shape[0]
     all_corrs = np.zeros((n_features, n_prfs, n_sem_axes), dtype=np.float32)
     all_discrim =  np.zeros((n_features, n_prfs, n_sem_axes), dtype=np.float32)
     all_mean = np.zeros((n_features, n_prfs), dtype=np.float32)
@@ -109,39 +130,17 @@ def get_feature_discrim(subject, feature_type, which_prf_grid=1, debug=False):
             continue
 
         print('Processing pRF %d of %d'%(prf_model_index, n_prfs))
-        if prf_model_index not in prf_inds_loaded:
+        
+        for si, feat_loader in enumerate(feat_loaders):
 
-            batch_to_use = np.where([prf_model_index in prf_batch_inds[bb] for \
-                                     bb in range(len(prf_batch_inds))])[0][0]
-            assert(prf_model_index in prf_batch_inds[batch_to_use])
+            # take training set trials only
+            features_ss, _ = feat_loader.load(np.where(trninds_list[si])[0],prf_model_index);
 
-            features_each_prf_batch = None
+            if si==0:
+                features_in_prf_trn = features_ss
+            else:
+                features_in_prf_trn = np.concatenate([features_in_prf_trn,features_ss], axis=0)
 
-            for si, features_file in enumerate(features_files):
-                print('Loading pre-computed features for prf models [%d - %d] from %s'%\
-                      (prf_batch_inds[batch_to_use][0],prf_batch_inds[batch_to_use][-1], features_file))
-                
-                t = time.time()
-                with h5py.File(features_file, 'r') as data_set:
-                    values = np.copy(data_set['/features'][:,:,prf_batch_inds[batch_to_use]])
-                    data_set.close() 
-                elapsed = time.time() - t
-                print('Took %.5f seconds to load file'%elapsed)
-
-                # take training set trials only
-                features_ss = values.astype(np.float32)[trninds_list[si],:,:]
-                values=None
-                
-                if si==0:
-                    features_each_prf_batch_trn = features_ss
-                else:
-                    features_each_prf_batch_trn = np.concatenate([features_each_prf_batch_trn,features_ss], axis=0)
-                    
-            prf_inds_loaded = prf_batch_inds[batch_to_use]
-                                 
-        index_into_batch = np.where(prf_model_index==prf_inds_loaded)[0][0]
-        print('Index into batch for prf %d: %d'%(prf_model_index, index_into_batch))
-        features_in_prf_trn = features_each_prf_batch_trn[:,:,index_into_batch]
         assert(features_in_prf_trn.shape[0]==labels_all.shape[0])
         print('Size of features array for this image set and prf is:')
         print(features_in_prf_trn.shape)
@@ -216,11 +215,13 @@ if __name__ == '__main__':
                     help="want to run a fast test version of this script to debug? 1 for yes, 0 for no")
     parser.add_argument("--which_prf_grid", type=int,default=1,
                     help="which prf grid to use")
+    parser.add_argument("--layer_name", type=str,default='',
+                    help="which DNN layer to use (if clip or alexnet)")
    
     args = parser.parse_args()
 
     if args.debug:
         print('DEBUG MODE\n')
 
-    get_feature_discrim(subject=args.subject, feature_type=args.feature_type, debug=args.debug==1, which_prf_grid=args.which_prf_grid)
+    get_feature_discrim(subject=args.subject, feature_type=args.feature_type, debug=args.debug==1, which_prf_grid=args.which_prf_grid, layer_name=args.layer_name)
    

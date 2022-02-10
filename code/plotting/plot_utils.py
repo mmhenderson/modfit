0133f2a40a16530132bae4c59beafc329024a1bb
Margaret Marie Henderson <mmhender@cmu.edu>
2022-02-09 21:57:47 -0500

    modified pycortex map plotting code to take roi_def as input

--- code/plotting/plot_utils.py
+++ code/plotting/plot_utils.py
@@ -17,7 +17,8 @@ def set_all_font_sizes(fs):
 
 def get_full_surface(values, voxel_mask):
     """
-    For PyCortex: Put values for voxels that were analyzed back into their correct coordinates in full surface space matrix.
+    For PyCortex: Put values for voxels that were analyzed back into their 
+    correct coordinates in full surface space matrix.
     """
     full_vals = copy.deepcopy(voxel_mask).astype('float64')
     full_vals[voxel_mask==0] = np.nan
@@ -27,7 +28,8 @@ def get_full_surface(values, voxel_mask):
 
 def get_full_volume(values, voxel_mask, shape):
     """
-    For PyCortex: Put values for voxels that were analyzed back into their correct coordinates in full surface space matrix.
+    For PyCortex: Put values for voxels that were analyzed back into their 
+    correct coordinates in full volume space matrix.
     """
     voxel_mask_3d = np.reshape(voxel_mask, shape)
     full_vals = copy.deepcopy(voxel_mask_3d).astype('float64')
@@ -38,65 +40,108 @@ def get_full_volume(values, voxel_mask, shape):
     
     return full_vals
 
-
-def plot_maps_pycortex(maps, names, subject, out, fitting_type, port, vox2plot = None, roi_def=None, \
-                       mins=None, maxes=None, cmaps=None):
-
+def get_roi_maps_for_pycortex(subject, roi_def):    
     """
-    Plot some maps in pycortex with desired specifications. Also add roi plots as additional layers.
-    """
+    Create a dictionary of cortex.Vertex labels for ROIs, to be plotted in PyCortex.
+    roi_def is from roi_utils.nsd_roi_def()
+    """ 
+    substr = 'subj%02d'%subject
     
-    if 'volume_space' in list(out.keys()):
-        volume_space = out['volume_space']
-    elif out['brain_nii_shape'] is not None:
-        volume_space = True
-    else:
-        volume_space = False
-        
-    if roi_def is None:
-        roi_def = roi_utils.get_combined_rois(subject,verbose=False)
-    retlabs, facelabs, placelabs, bodylabs, ret_names, face_names, place_names, body_names = roi_def    
-
+    retlabs = roi_def.retlabs
+    facelabs = roi_def.facelabs
+    placelabs = roi_def.placelabs
+    bodylabs = roi_def.bodylabs
     retlabs[retlabs==-1] = np.nan
     facelabs[facelabs==-1] = np.nan
     placelabs[placelabs==-1] = np.nan
     bodylabs[bodylabs==-1] = np.nan
-    substr = 'subj%02d'%subject
 
-    voxel_mask = out['voxel_mask']
-    
+    volume_space = roi_def.volume_space
+    voxel_mask = roi_def.voxel_mask
+    nii_shape = roi_def.nii_shape
+
     if volume_space:
         print('Data is in 3d volume space')
         xfmname = 'func1pt8_to_anat0pt8_autoFSbbr'
-        nii_shape = out['brain_nii_shape']
         mask_3d = np.reshape(voxel_mask, nii_shape, order='C')
-        
+
         dat2plot = {'ROI labels (retinotopic)': \
-                cortex.Volume(data=get_full_volume(retlabs, voxel_mask, nii_shape),\
-                subject=substr, cmap='Accent',vmin = 0, vmax = np.max(retlabs[~np.isnan(retlabs)])+1,\
-                xfmname=xfmname, mask=mask_3d), \
-                'ROI labels (face-selective)': cortex.Volume(data=get_full_volume(facelabs, voxel_mask, nii_shape),\
-                 subject=substr, cmap='Accent',vmin = 0, vmax = np.max(facelabs[~np.isnan(facelabs)])+1, \
-                 xfmname=xfmname, mask=mask_3d), \
-                'ROI labels (place-selective)': cortex.Volume(data=get_full_volume(placelabs, voxel_mask, nii_shape),\
-                 subject=substr, cmap='Accent',vmin = 0, vmax = np.max(placelabs[~np.isnan(placelabs)])+1, \
-                 xfmname=xfmname, mask=mask_3d), \
-                'ROI labels (body-selective)': cortex.Volume(data=get_full_volume(bodylabs, voxel_mask, nii_shape),\
-                 subject=substr, cmap='Accent',vmin = 0, vmax = np.max(bodylabs[~np.isnan(bodylabs)])+1, \
-                 xfmname=xfmname, mask=mask_3d)}
-       
+                    cortex.Volume(data=get_full_volume(retlabs, voxel_mask, nii_shape),\
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(retlabs[~np.isnan(retlabs)])+1,\
+                    xfmname=xfmname, mask=mask_3d), \
+                'ROI labels (face-selective)': \
+                    cortex.Volume(data=get_full_volume(facelabs, voxel_mask, nii_shape),\
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(facelabs[~np.isnan(facelabs)])+1, \
+                    xfmname=xfmname, mask=mask_3d), \
+                'ROI labels (place-selective)': \
+                    cortex.Volume(data=get_full_volume(placelabs, voxel_mask, nii_shape),\
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(placelabs[~np.isnan(placelabs)])+1, \
+                    xfmname=xfmname, mask=mask_3d), \
+                'ROI labels (body-selective)': \
+                    cortex.Volume(data=get_full_volume(bodylabs, voxel_mask, nii_shape),\
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(bodylabs[~np.isnan(bodylabs)])+1, \
+                     xfmname=xfmname, mask=mask_3d)}
+
     else:
         print('Data is in nativesurface space')
 
-        dat2plot = {'ROI labels (retinotopic)': cortex.Vertex(data = get_full_surface(retlabs, voxel_mask), \
-                                                    subject=substr, cmap='Accent',vmin = 0, vmax = np.max(retlabs)+1), \
-                'ROI labels (face-selective)': cortex.Vertex(data = get_full_surface(facelabs, voxel_mask), \
-                                                    subject=substr, cmap='Accent',vmin = 0, vmax = np.max(facelabs)+1), \
-                'ROI labels (place-selective)': cortex.Vertex(data = get_full_surface(placelabs, voxel_mask), \
-                                                    subject=substr, cmap='Accent',vmin = 0, vmax = np.max(placelabs)+1), \
-                'ROI labels (body-selective)': cortex.Vertex(data = get_full_surface(bodylabs, voxel_mask), \
-                                                    subject=substr, cmap='Accent',vmin = 0, vmax = np.max(bodylabs)+1)}
-                
+        dat2plot = {'ROI labels (retinotopic)': \
+                    cortex.Vertex(data = get_full_surface(retlabs, voxel_mask), \
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(retlabs)+1), \
+                'ROI labels (face-selective)': \
+                    cortex.Vertex(data = get_full_surface(facelabs, voxel_mask), \
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(facelabs)+1), \
+                'ROI labels (place-selective)': \
+                    cortex.Vertex(data = get_full_surface(placelabs, voxel_mask), \
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(placelabs)+1), \
+                'ROI labels (body-selective)': \
+                    cortex.Vertex(data = get_full_surface(bodylabs, voxel_mask), \
+                    subject=substr, cmap='Accent',\
+                    vmin = 0, vmax = np.max(bodylabs)+1)}
+        
+    return dat2plot
+
+    
+def plot_maps_pycortex(subject, title, port, maps, names, roi_def=None, \
+                       mins=None, maxes=None, cmaps=None, vox2plot = None, \
+                      volume_space=None, nii_shape=None, voxel_mask=None):
+
+    """
+    Plot a set of maps in PyCortex surface space.
+    Need to either specify a voxel_mask (which voxels in whole brain do your maps 
+    correspond to?) or include roi_def (an object from roi_utils.nsd_roi_def that includes
+    info about all the voxels/rois).
+    """
+    
+    substr = 'subj%02d'%subject
+  
+    if roi_def is not None:
+        dat2plot = get_roi_maps_for_pycortex(subject, roi_def)
+        volume_space = roi_def.volume_space
+        voxel_mask = roi_def.voxel_mask
+        nii_shape = roi_def.nii_shape
+    else:
+        if voxel_mask is None:
+            raise ValueError('must specify either voxel_mask or roi_def.')   
+        if volume_space is None:
+            volume_space = nii_shape is not None
+        else:
+            if nii_shape is None:
+                raise ValueError('must specify nii_shape if volume_space=True.')   
+        dat2plot = {}
+        
+    if volume_space:
+        xfmname = 'func1pt8_to_anat0pt8_autoFSbbr'
+        mask_3d = np.reshape(voxel_mask, nii_shape, order='C')
+
+        
     if mins is None:
         mins = [None for ll in range(len(names))]
     elif len(mins)==1:
@@ -116,17 +161,18 @@ def plot_maps_pycortex(maps, names, subject, out, fitting_type, port, vox2plot =
             map_thresh[~vox2plot] = np.nan
         if volume_space:
             dat2plot[name] = cortex.Volume(data = get_full_volume(map_thresh, voxel_mask, nii_shape), \
-                                                    cmap=cmaps[ni], subject=substr, vmin=mins[ni], vmax=maxes[ni],\
+                                           cmap=cmaps[ni], subject=substr, \
+                                           vmin=mins[ni], vmax=maxes[ni],\
                                            xfmname=xfmname, mask=mask_3d)
         else:
-            dat2plot[name] = cortex.Vertex(data = get_full_surface(map_thresh, voxel_mask), \
-                                                    cmap=cmaps[ni], subject=substr, vmin=mins[ni], vmax=maxes[ni])
+            dat2plot[name] = cortex.Vertex(data = get_full_surface(map_thresh, voxel_mask),\
+                                           cmap=cmaps[ni], subject=substr, \
+                                           vmin=mins[ni], vmax=maxes[ni])
             
     # Open the webviewer
     print('navigate browser to: 127.0.0.1:%s'%port)
-    overlay_file = 'overlays.svg'
-    cortex.webshow(dat2plot, open_browser=True, port=port, title = 'S%02d, %s'%(subject, fitting_type), overlay_file=overlay_file, \
-                  overlays_visible=('rois', 'sulci'))
+    cortex.webshow(dat2plot, open_browser=True, port=port, \
+                   title = 'S%02d, %s'%(subject, title))
     
 
     

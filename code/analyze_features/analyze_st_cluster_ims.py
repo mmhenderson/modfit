d74665e32699e0b6403905f80d87c069a7f0d840
Margaret Marie Henderson <mmhender@cmu.edu>
2022-03-29 11:44:55 -0400

    updating cluster image analysis

--- code/analyze_features/analyze_st_cluster_ims.py
+++ code/analyze_features/analyze_st_cluster_ims.py
@@ -14,6 +14,7 @@ def process_cluster_ims(debug=False):
     """
     For each of the sketch tokens cluster images, quantify the approximate orientation and 
     curvature of the feature. Save result as a csv file.
+    Orientation values will go in a counter-clockwise direction starting from vertical.
     """
     
     if not debug:
@@ -30,7 +31,13 @@ def process_cluster_ims(debug=False):
     cluster_fn = os.path.join('/user_data/mmhender/toolboxes/SketchTokens/', 'clusters.mat')
     clust = loadmat(cluster_fn)
     feature_ims = clust['clusters']['clusters'][0][0]
+    # these features ims were made in matlab, so the X and Y dims are swapped. 
+    # Switch them back now before processing.
+    feature_ims = np.moveaxis(feature_ims, [0,1,2], [1,0,2])
     n_clusters = feature_ims.shape[2]
+    
+    # qualitative groupings for the features
+    grouping_df = get_simple_groupings()
     
     # define a set of features to test 
     bends = np.linspace(0, 0.30, 8)
@@ -57,36 +64,24 @@ def process_cluster_ims(debug=False):
     # filter with bent "banana" gabors
     all_curv_filt_coeff = bank.filter_image_batch_pytorch(feature_ims_trimmed, which_kernels='curv')
     
-    # find the kernel that gives the max activation in the image center
-    # (the cluster images are centered, so expect the best match to be close to center)
-    center = int(cropped_size/2)
-    npix_out = 3;
-    center_bbox = [center-npix_out, center+npix_out, center-npix_out, center+npix_out]
-    
-    lin_center = all_lin_filt_coeff[center_bbox[0]:center_bbox[1], \
-                                    center_bbox[2]:center_bbox[3]]
-    curv_center = all_curv_filt_coeff[center_bbox[0]:center_bbox[1], \
-                                    center_bbox[2]:center_bbox[3]]
-    
-    max_lin_center = np.max(np.max(lin_center, axis=0), axis=0)
-    max_curv_center = np.max(np.max(curv_center, axis=0), axis=0)
-    
-    # for the linear filters, save the full profile of activation over orientation
-    max_lin_whole_image = np.max(np.max(all_lin_filt_coeff, axis=0), axis=0)
-    features_use = bank.lin_kernel_pars[:,0]==np.min(bank.lin_kernel_pars[:,0])
-    max_activ_each_orient = max_lin_whole_image[features_use,:].T
-    
-    
-    # identify which feature led to the best activ, and what the activation value was
-    best_lin_kernel = np.argmax(max_lin_center, axis=0)
-    max_lin_activ = np.max(max_lin_center, axis=0)
+    # find max activation of each filter, across each image
+    max_lin_each_image = np.max(np.max(all_lin_filt_coeff, axis=0), axis=0)
+    max_curv_each_image = np.max(np.max(all_curv_filt_coeff, axis=0), axis=0)
+  
+    # identify which filter led to the best activ, and what the activation value was
+    best_lin_kernel = np.argmax(max_lin_each_image, axis=0)
+    max_lin_activ = np.max(max_lin_each_image, axis=0)
    
-    best_curv_kernel = np.argmax(max_curv_center, axis=0)
-    max_curv_activ = np.max(max_curv_center, axis=0)
+    best_curv_kernel = np.argmax(max_curv_each_image, axis=0)
+    max_curv_activ = np.max(max_curv_each_image, axis=0)
    
     # was the overall best kernel a curved or linear gabor?
     which_best = np.argmax(np.array([max_lin_activ, max_curv_activ]), axis=0)
-
+    
+    # for the double line and junction features, only look at the linear filters
+    which_best[grouping_df['double']==True] = 0
+    which_best[grouping_df['junction']==True] = 0
+    
     # make a table to save things
     best_scale = bank.lin_kernel_pars[best_lin_kernel,0]
     best_bend = bank.lin_kernel_pars[best_lin_kernel,1]
@@ -97,12 +92,9 @@ def process_cluster_ims(debug=False):
     best_bend[which_best==1] = \
         bank.curv_kernel_pars[best_curv_kernel[which_best==1],1]
     best_orient[which_best==1] = \
-        bank.curv_kernel_pars[best_curv_kernel[which_best==1],2]
-     
+        bank.curv_kernel_pars[best_curv_kernel[which_best==1],2]     
     best_freq = best_scale * cropped_size / (2*np.pi)
-    
-    grouping_df = get_simple_groupings()
-    
+      
     quant_df = pd.DataFrame({'best_freq': best_freq, 'best_scale': best_scale, \
                              'best_bend': best_bend, 'best_orient': best_orient, \
                              'best_linear_index': best_lin_kernel, 'best_curv_index': best_curv_kernel, \
@@ -115,12 +107,6 @@ def process_cluster_ims(debug=False):
     print('saving to %s'%fn2save)
     final_df.to_csv(fn2save,index=False)
     
-    fn2save = os.path.join(default_paths.sketch_token_feat_path,\
-                           'Sketch_tokens_orient_profiles.npy')
-    print('saving to %s'%fn2save)
-    np.save(fn2save, max_activ_each_orient)
-
-
 def get_simple_groupings():
     
     # qualitative groupings of the sketch tokens features, based on whether they are 
@@ -129,12 +115,17 @@ def get_simple_groupings():
     double_lines = [17, 27, 30, 32, 33, 34, 35, 40, 51, 59, 60, 61, 64, 65, 66, 68, 69, 73, 75, 78, 80, 84, \
                       88, 90, 96, 106, 108, 110, 111, 113, 114, 132, 133, 134, 136]
     is_double = np.isin(np.arange(150), double_lines)
-    junction = [38, 62, 63, 67, 70, 71, 77, 79, 81, 86, 94, 100, 102, 103, 112, 115, 116, 118, 119, 120, 124, 125, 126, \
+    junction = [38, 62, 63, 67, 70, 77, 79, 81, 86, 94, 100, 102, 103, 112, 115, 116, 118, 119, 120, 124, 125, 126, \
                137, 138, 140, 142, 143, 144, 145, 146, 149]
     is_junction = np.isin(np.arange(150), junction)
-    is_single = ~is_double &  ~is_junction
+    # "other" is one feature that is not like the others, more texture-like
+    other = [107]
+    is_other = np.isin(np.arange(150), other)
+    
+    assert(not np.any(is_double & is_junction) and not np.any(is_double & is_other) and not np.any(is_junction & is_other))
+    is_single = ~is_double &  ~is_junction & ~is_other
 
-    grouping_df = pd.DataFrame({'double': is_double, 'junction': is_junction, 'single': is_single})
+    grouping_df = pd.DataFrame({'double': is_double, 'junction': is_junction, 'single': is_single, 'other': is_other})
     
     return grouping_df
 

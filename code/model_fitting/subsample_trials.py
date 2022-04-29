1a0832296e176157c0cd5133bdba56502bc20fcb
Margaret Marie Henderson <mmhender@cmu.edu>
2022-04-29 11:56:47 -0400

    balance trial order for frequency/category

--- code/model_fitting/subsample_trials.py
+++ code/model_fitting/subsample_trials.py
@@ -180,6 +180,179 @@ def balance_orient_vs_categories(subject, which_prf_grid=5, axes_to_do=[0,2,3], 
                           'rnd_seed': rnd_seed}, 
                 allow_pickle=True)
         
+
+def balance_freq_vs_categories(subject, which_prf_grid=5, axes_to_do=[0,2,3], n_samp_iters=1000, \
+                                 debug=False):
+   
+    models = initialize_fitting.get_prf_models(which_grid = which_prf_grid)   
+    n_prfs = models.shape[0]
+
+    # figure out what images are available for this subject - assume that we 
+    # will be using all the available sessions. 
+    image_order = nsd_utils.get_master_image_order()    
+    session_inds = nsd_utils.get_session_inds_full()
+    sessions = np.arange(nsd_utils.max_sess_each_subj[subject-1])
+    inds2use = np.isin(session_inds, sessions) # remove any sessions that weren't shown
+    # list of all the image indices shown on each trial
+    image_order = image_order[inds2use] 
+    # reduce to the 10,000 unique images
+    # NOTE that this order will only work correctly if we average over image repetitions
+    image_order = np.unique(image_order) 
+
+    # load the list of which images are training, holdout, and validation
+    # each is a mask [10,000] long
+    is_trn, is_holdout, is_val = nsd_utils.load_image_data_partitions(subject)
+    
+    trninds = is_trn[image_order]
+    valinds = is_val[image_order]
+    outinds = is_holdout[image_order]
+    
+    n_trials = len(image_order)
+    n_trn_trials = np.sum(trninds)
+    n_out_trials = np.sum(outinds)
+    n_val_trials = np.sum(valinds)
+    
+    # get semantic category labels
+    labels_all, discrim_type_list, unique_labels_each = \
+                        initialize_fitting.load_labels_each_prf(subject, \
+                                which_prf_grid, image_inds=image_order, \
+                                models=models,verbose=False, debug=debug)
+    labels_all_trn = labels_all[trninds,:,:]
+    labels_all_val = labels_all[valinds,:,:]
+    labels_all_out = labels_all[outinds,:,:]
+    
+    # create feature loader
+    feat_loaders, path_to_load = \
+        default_feature_loaders.get_feature_loaders([subject], \
+                                                    feature_type='gabor_solo',\
+                                                    which_prf_grid=which_prf_grid)
+    feat_loader = feat_loaders[0]
+
+    # boolean masks for which trials will be included in the balanced sets
+    trninds_mask = np.zeros((n_trn_trials,n_samp_iters,n_prfs,len(axes_to_do)), dtype=bool)
+    valinds_mask = np.zeros((n_val_trials,n_samp_iters,n_prfs,len(axes_to_do)), dtype=bool)
+    outinds_mask = np.zeros((n_out_trials,n_samp_iters,n_prfs,len(axes_to_do)), dtype=bool)
+  
+    min_counts_trn = np.zeros((n_prfs,len(axes_to_do)))
+    min_counts_val = np.zeros((n_prfs,len(axes_to_do)))
+    min_counts_out = np.zeros((n_prfs,len(axes_to_do)))
+
+    # will put the 8 spat freq into four equal sized bins. 
+    # n_freq_bins=4;
+    n_freq_bins=2;
+    # bin_values = np.array([0,0,1,1,2,2,3,3],dtype=int);
+    # bin_values = np.array([0,0,0,0,1,1,1,1],dtype=int);
+    bin_values = np.array([0,0,1,1,1,1,1,1],dtype=int);
+    unique_freq = np.arange(n_freq_bins);
+    n_ori=12;n_sf=8;
+    
+    n_categ=2;
+    unique_categ = np.arange(n_categ);
+    
+    rnd_seed = int(time.strftime('%M%H%d', time.localtime()))
+    np.random.seed(rnd_seed)
+    
+    for mm in range(n_prfs):
+
+        if debug and mm>1:
+            continue
+
+        print('processing pRF %d of %d'%(mm, n_prfs))
+        sys.stdout.flush()
+        # load features for this set of images 
+        features, _ = feat_loader.load(image_order,mm);
+        features_reshaped = np.reshape(features, [n_trials, n_ori, n_sf], order='F')
+        features_each_freq = np.mean(features_reshaped, axis=1)
+        
+        max_freq = np.argmax(features_each_freq, axis=1).astype(int)
+        freq_labels = bin_values[max_freq]
+        freq_labels_trn = freq_labels[trninds]
+        freq_labels_val = freq_labels[valinds]
+        freq_labels_out = freq_labels[outinds]
+   
+        for ai, aa in enumerate(axes_to_do):
+
+            print([ai, aa])
+            # labels for whatever semantic axis is of interest
+            categ_labels_trn = labels_all_trn[:,aa,mm]  
+            categ_labels_val = labels_all_val[:,aa,mm]  
+            categ_labels_out = labels_all_out[:,aa,mm]  
+
+            trial_inds_resample_trn, min_count_trn = \
+                    get_balanced_trials(freq_labels_trn, \
+                                        categ_labels_trn, n_samp_iters=n_samp_iters, \
+                                        unique1=unique_freq,unique2=unique_categ)
+            trial_inds_resample_val, min_count_val = \
+                    get_balanced_trials(freq_labels_val, \
+                                        categ_labels_val, n_samp_iters=n_samp_iters, \
+                                        unique1=unique_freq,unique2=unique_categ)
+            trial_inds_resample_out, min_count_out = \
+                    get_balanced_trials(freq_labels_out, \
+                                        categ_labels_out, n_samp_iters=n_samp_iters, \
+                                        unique1=unique_freq,unique2=unique_categ)
+            
+            # check a few of the trial lists just to make sure this worked
+            if min_count_trn is not None:
+                u, counts = np.unique(freq_labels_trn[trial_inds_resample_trn[1,:]], return_counts=True)
+                assert(np.all(u==unique_freq))
+                assert(np.all(counts==min_count_trn*n_categ))
+                u, counts = np.unique(categ_labels_trn[trial_inds_resample_trn[1,:]], return_counts=True)
+                assert(np.all(u==unique_categ))
+                assert(np.all(counts==min_count_trn*n_freq_bins))
+                
+            if min_count_val is not None:
+                u, counts = np.unique(freq_labels_val[trial_inds_resample_val[1,:]], return_counts=True)
+                assert(np.all(u==unique_freq))
+                assert(np.all(counts==min_count_val*n_categ))
+                u, counts = np.unique(categ_labels_val[trial_inds_resample_val[1,:]], return_counts=True)
+                assert(np.all(u==unique_categ))
+                assert(np.all(counts==min_count_val*n_freq_bins))
+                
+            if min_count_out is not None:
+                u, counts = np.unique(freq_labels_out[trial_inds_resample_out[1,:]], return_counts=True)
+                assert(np.all(u==unique_freq))
+                assert(np.all(counts==min_count_out*n_categ))
+                u, counts = np.unique(categ_labels_out[trial_inds_resample_out[1,:]], return_counts=True)
+                assert(np.all(u==unique_categ))
+                assert(np.all(counts==min_count_out*n_freq_bins))
+
+            # put the numeric trial indices into boolean mask arrays
+            for xx in range(n_samp_iters):
+                if trial_inds_resample_trn is not None:
+                    trninds_mask[trial_inds_resample_trn[xx,:],xx,mm,ai] = 1
+                if trial_inds_resample_val is not None:
+                    valinds_mask[trial_inds_resample_val[xx,:],xx,mm,ai] = 1
+                if trial_inds_resample_out is not None:
+                    outinds_mask[trial_inds_resample_out[xx,:],xx,mm,ai] = 1
+
+            min_counts_trn[mm,ai] = min_count_trn
+            min_counts_val[mm,ai] = min_count_val
+            min_counts_out[mm,ai] = min_count_out
+
+    # saving the results, one file per semantic axis of interest
+    for ai, aa in enumerate(axes_to_do):
+        print([ai, aa])
+        print(discrim_type_list[aa])
+        fn2save = os.path.join(default_paths.stim_labels_root, 'resampled_trial_orders', \
+                           'S%d_trial_resamp_order_balance_2freqbins_%s.npy'\
+                               %(subject, discrim_type_list[aa]))
+        # fn2save = os.path.join(default_paths.stim_labels_root, 'resampled_trial_orders', \
+        #                    'S%d_trial_resamp_order_balance_4freqbins_%s.npy'\
+        #                        %(subject, discrim_type_list[aa]))
+        print('saving to %s'%fn2save)
+        np.save(fn2save, {'trial_inds_trn': trninds_mask[:,:,:,ai], \
+                          'min_counts_trn': min_counts_trn[:,ai], \
+                          'trial_inds_val': valinds_mask[:,:,:,ai], \
+                          'min_counts_val': min_counts_val[:,ai], \
+                          'trial_inds_out': outinds_mask[:,:,:,ai], \
+                          'min_counts_out': min_counts_out[:,ai], \
+                          'image_order': image_order, \
+                          'trninds': trninds, \
+                          'valinds': valinds, \
+                          'outinds': outinds, \
+                          'rnd_seed': rnd_seed}, 
+                allow_pickle=True)
+        
 def get_balanced_trials(labels1, labels2, n_samp_iters=1000, \
                         unique1=None, unique2=None, rndseed=None):
     
@@ -479,6 +652,8 @@ if __name__ == '__main__':
                     help="which grid of candidate prfs?")     
     parser.add_argument("--debug",type=int,default=0,
                     help="want to run a fast test version of this script to debug? 1 for yes, 0 for no")
+    parser.add_argument("--balance_freq_categ",type=int,default=0,
+                    help="want to create labels balanced for freq/categ? 1 for yes, 0 for no")
     parser.add_argument("--balance_orient_categ",type=int,default=0,
                     help="want to create labels balanced for orient/categ? 1 for yes, 0 for no")
     parser.add_argument("--separate_categ",type=int,default=0,
@@ -490,6 +665,11 @@ if __name__ == '__main__':
     
     if args.balance_orient_categ:
         balance_orient_vs_categories(args.subject, args.which_prf_grid, \
+                                 axes_to_do=[0,2,3], debug=args.debug==1, \
+                                 n_samp_iters=args.n_samp_iters)
+                       
+    if args.balance_freq_categ:
+        balance_freq_vs_categories(args.subject, args.which_prf_grid, \
                                  axes_to_do=[0,2,3], debug=args.debug==1, \
                                  n_samp_iters=args.n_samp_iters)
     if args.separate_categ:

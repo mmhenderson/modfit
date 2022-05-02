0c1b3f9ece20eca7735f62d5ecb408000e8f1329
Margaret Marie Henderson <mmhender@cmu.edu>
2022-05-01 22:41:22 -0400

    add option to skip overlapping voxels when computing prf coverage

--- code/analyze_fits/analyze_prfs.py
+++ code/analyze_fits/analyze_prfs.py
@@ -4,7 +4,8 @@ import numpy as np
 from plotting import load_fits
 from utils import roi_utils, default_paths, prf_utils
 
-def compute_prf_coverage(subjects, fitting_type='alexnet_all_conv_pca', image_size=224):
+def compute_prf_coverage(subjects, fitting_type='alexnet_all_conv_pca', image_size=224, \
+                        ignore_overlapping_voxels=True):
     
     n_subjects = len(subjects)
     out = [load_fits.load_fit_results(subject=ss, \
@@ -27,7 +28,10 @@ def compute_prf_coverage(subjects, fitting_type='alexnet_all_conv_pca', image_si
     val_r2 = np.concatenate([out[si]['val_r2'][:,0] for si in range(n_subjects)], axis=0)
     abv_thresh = val_r2>r2_cutoff
 
-    roi_def = roi_utils.multi_subject_roi_def(subjects)
+    if ignore_overlapping_voxels:
+        roi_def = roi_utils.multi_subject_roi_def(subjects, remove_ret_overlap=True, remove_categ_overlap=True)
+    else:
+        roi_def = roi_utils.multi_subject_roi_def(subjects)
     roi_names =roi_def.roi_names
     n_rois = roi_def.n_rois
     
@@ -58,7 +62,12 @@ def compute_prf_coverage(subjects, fitting_type='alexnet_all_conv_pca', image_si
             all_max_prfs[:,:,rr,si] = np.max(roi_prfs, axis=2)
             all_mean_prfs[:,:,rr,si] = np.mean(roi_prfs, axis=2)
 
-    fn2save = os.path.join(default_paths.save_fits_path, 'prf_coverage', 'All_pRFs_%s.npy'%fitting_type)
+    if ignore_overlapping_voxels:
+        fn2save = os.path.join(default_paths.save_fits_path, 'prf_coverage', \
+                               'All_pRFs_%s_no_roi_overlap_%dpix.npy'%(fitting_type, image_size))       
+    else:
+        fn2save = os.path.join(default_paths.save_fits_path, 'prf_coverage', \
+                               'All_pRFs_%s_%dpix.npy'%(fitting_type, image_size))
     print('saving to %s'%fn2save)
     np.save(fn2save, {'all_max_prfs': all_max_prfs, \
                       'all_mean_prfs': all_mean_prfs, \

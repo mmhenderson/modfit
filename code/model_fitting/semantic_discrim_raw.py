2f794cff8db3d39c609dd393926b610b1d448584
Margaret Marie Henderson <mmhender@cmu.edu>
2022-06-03 11:45:12 -0400

    fix a bug

--- code/model_fitting/semantic_discrim_raw.py
+++ code/model_fitting/semantic_discrim_raw.py
@@ -213,7 +213,9 @@ if __name__ == '__main__':
     
     parser.add_argument("--which_prf_grid", type=int,default=1,
                     help="which grid of candidate prfs?")
-     
+    parser.add_argument("--prfs_model_name", type=str, default='', 
+                    help="model the prfs are from?")
+    
     parser.add_argument("--debug",type=nice_str2bool,default=False,
                     help="want to run a fast test version of this script to debug? 1 for yes, 0 for no")
     

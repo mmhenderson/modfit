6fd9254baf37e8f6e9dba9a3a82a3b70485d375e
Margaret Marie Henderson <mmhender@cmu.edu>
2022-07-29 17:19:51 -0400

    add option to save the weights from PCA, and option to load saved weights and apply to a new dataset

--- code/feature_extraction/pca_feats.py
+++ code/feature_extraction/pca_feats.py
@@ -20,6 +20,7 @@ def run_pca_each_prf(raw_filename, pca_filename, \
               zscore_before_pca = False, \
               zgroup_labels = None,\
               min_pct_var=95, max_pc_to_retain=100,\
+              save_weights=False, save_weights_filename=None,\
               save_dtype=np.float32, compress=True, \
               debug=False):
 
@@ -48,9 +49,15 @@ def run_pca_each_prf(raw_filename, pca_filename, \
     if not batches_in_separate_files:
         n_prf_batches = int(np.ceil(n_prfs/prf_batch_size))  
     
+    print('max_pc_to_retain:')
+    print(max_pc_to_retain, n_feat_orig)
+    
     if max_pc_to_retain is None:
         max_pc_to_retain = n_feat_orig
-        
+    else:
+        max_pc_to_retain = np.minimum(max_pc_to_retain, n_feat_orig)
+    print(max_pc_to_retain)
+    
     if fit_inds is not None:
         assert(len(fit_inds)==n_trials)
     else:
@@ -66,6 +73,12 @@ def run_pca_each_prf(raw_filename, pca_filename, \
         
     scores_each_prf = np.zeros((n_trials, max_pc_to_retain, n_prfs), dtype=save_dtype)
     actual_max_ncomp=0
+    
+    if save_weights:
+        pca_wts = np.zeros((n_feat_orig, max_pc_to_retain, n_prfs), dtype=save_dtype)
+        pca_premean = np.zeros((n_feat_orig, n_prfs), dtype=save_dtype)
+        pca_ncomp = np.zeros((n_prfs,),dtype=save_dtype)
+        assert(save_weights_filename is not None)
        
     for prf_model_index in range(n_prfs):
 
@@ -132,7 +145,7 @@ def run_pca_each_prf(raw_filename, pca_filename, \
         print('any nans in array: %s'%np.any(np.isnan(features_in_prf)))
         
         # finding pca solution for just training data (specified in fit_inds)
-        _, wts, pre_mean, ev = do_pca(features_in_prf[fit_inds,:], max_pc_to_retain=max_pc_to_retain)
+        _, wts, pre_mean, ev = compute_pca(features_in_prf[fit_inds,:], max_pc_to_retain=max_pc_to_retain)
 
         # now projecting all the data incl. val into same subspace
         feat_submean = features_in_prf - np.tile(pre_mean[np.newaxis,:], [features_in_prf.shape[0],1])
@@ -144,6 +157,160 @@ def run_pca_each_prf(raw_filename, pca_filename, \
         else:
             n_comp_needed = scores.shape[1]
         print('Retaining %d components to explain %d pct var'%(n_comp_needed, min_pct_var))
+        actual_max_ncomp = np.max([n_comp_needed, actual_max_ncomp])
+        
+        scores_each_prf[:,0:n_comp_needed,prf_model_index] = scores[:,0:n_comp_needed]
+        scores_each_prf[:,n_comp_needed:,prf_model_index] = np.nan
+
+        if save_weights:
+            pca_wts[:,:,prf_model_index] = wts.T
+            pca_premean[:,prf_model_index] = pre_mean
+            pca_ncomp[prf_model_index] = n_comp_needed
+            
+    # To save space, get rid of portion of array that ended up all nans
+    if debug:
+        actual_max_ncomp=np.max([2,actual_max_ncomp])
+        assert(np.all((scores_each_prf[:,actual_max_ncomp:,:]==0) | np.isnan(scores_each_prf[:,actual_max_ncomp:,:])))
+    else:
+        assert(np.all(np.isnan(scores_each_prf[:,actual_max_ncomp:,:])))
+    scores_each_prf = scores_each_prf[:,0:actual_max_ncomp,:]
+    
+    print('final size of array to save:')
+    print(scores_each_prf.shape)    
+    print('saving to %s'%pca_filename)
+    
+    t = time.time()
+    
+    with h5py.File(pca_filename, 'w') as data_set:
+        if compress==True:
+            dset = data_set.create_dataset("features", np.shape(scores_each_prf), dtype=save_dtype, compression='gzip')
+        else:
+            dset = data_set.create_dataset("features", np.shape(scores_each_prf), dtype=save_dtype)
+        data_set['/features'][:,:,:] = scores_each_prf
+        data_set.close() 
+    elapsed = time.time() - t
+
+    print('Took %.5f sec to write file'%elapsed)
+      
+    if save_weights:
+        wts = {'pca_wts': pca_wts, 'pca_premean': pca_premean, 'pca_ncomp': pca_ncomp}
+        print('saving the weights for this pca to %s'%save_weights_filename)
+        np.save(save_weights_filename, wts, allow_pickle=True)
+
+        
+def apply_pca_each_prf(raw_filename, pca_filename, \
+                       load_weights_filename, \
+              prf_batch_size=50, \
+              zscore_before_pca = False, \
+              zgroup_labels = None,\
+              save_dtype=np.float32, compress=True, \
+              debug=False):
+
+    if isinstance(raw_filename, list):  
+        n_prf_batches = len(raw_filename) 
+        batches_in_separate_files=True        
+    else:
+        raw_filename = [raw_filename]      
+        batches_in_separate_files=False
+
+    print('loading pre-computed pca weights from %s'%(load_weights_filename))
+    w = np.load(load_weights_filename, allow_pickle=True).item()
+    pca_wts = w['pca_wts']
+    pca_premean = w['pca_premean']
+    pca_ncomp = w['pca_ncomp']
+    
+    n_prfs=0;
+    for fi, fn in enumerate(raw_filename):
+        if not os.path.exists(fn):
+            raise RuntimeError('Looking at %s for precomputed features, not found.'%fn)   
+        with h5py.File(fn, 'r') as data_set:
+            dsize = data_set['/features'].shape
+            data_set.close() 
+
+        n_trials, n_feat_orig, n_prfs_tmp = dsize
+        if fi==0 and batches_in_separate_files:
+            assert(n_prfs_tmp==prf_batch_size)
+        n_prfs += n_prfs_tmp
+    
+    if not batches_in_separate_files:
+        n_prf_batches = int(np.ceil(n_prfs/prf_batch_size))  
+    
+    max_pc_to_retain = n_feat_orig
+    # batching prfs for loading    
+    prf_batch_inds = [np.arange(prf_batch_size*bb, np.min([prf_batch_size*(bb+1), n_prfs])) \
+                          for bb in range(n_prf_batches)]
+    prf_inds_loaded = []
+    
+    if zscore_before_pca and zgroup_labels is None:
+        zgroup_labels = np.ones(shape=(1,n_feat_orig))
+        
+    scores_each_prf = np.zeros((n_trials, max_pc_to_retain, n_prfs), dtype=save_dtype)
+    actual_max_ncomp=0
+    
+    for prf_model_index in range(n_prfs):
+
+        if debug and prf_model_index>1:
+            continue
+
+        print('Processing pRF %d of %d'%(prf_model_index, n_prfs))
+
+        if prf_model_index not in prf_inds_loaded:
+
+            batch_to_use = np.where([prf_model_index in prf_batch_inds[bb] for \
+                                     bb in range(len(prf_batch_inds))])[0][0]
+            features_each_prf_batch = None
+            prf_inds_loaded = prf_batch_inds[batch_to_use]
+            
+            t = time.time()
+            
+            if batches_in_separate_files:
+                
+                # each batch of prfs is in a separate file here
+                filename_load = raw_filename[batch_to_use]
+                print('Loading pre-computed features for prf models [%d - %d] from %s'%\
+                  (prf_batch_inds[batch_to_use][0],prf_batch_inds[batch_to_use][-1], filename_load))
+                with h5py.File(filename_load, 'r') as data_set:
+                    values = np.copy(data_set['/features'][:,:,:])
+                    data_set.close() 
+                
+            else:
+                
+                # all prfs are in same file, can just load part of it
+                filename_load = raw_filename[0]
+                print('Loading pre-computed features for prf models [%d - %d] from %s'%\
+                    (prf_batch_inds[batch_to_use][0],prf_batch_inds[batch_to_use][-1], filename_load))
+                with h5py.File(filename_load, 'r') as data_set:
+                    values = np.copy(data_set['/features'][:,:,prf_batch_inds[batch_to_use]])
+                    data_set.close() 
+                
+            features_each_prf_batch = values
+
+        index_into_batch = np.where(prf_model_index==prf_inds_loaded)[0][0]
+        print('Index into batch for prf %d: %d'%(prf_model_index, index_into_batch))
+        features_in_prf = features_each_prf_batch[:,:,index_into_batch]
+
+        elapsed = time.time() - t
+        print('Took %.5f seconds to load file'%elapsed)
+        
+        if zscore_before_pca:
+            features_in_prf_z = numpy_utils.zscore_in_groups(features_in_prf, zgroup_labels)
+            zero_var = np.var(features_in_prf, axis=0)==0
+            features_in_prf_z[:,zero_var] = features_in_prf_z[0,zero_var]
+       
+            features_in_prf = features_in_prf_z
+        
+        print('Size of features array for this image set and prf is:')
+        print(features_in_prf.shape)
+        print('any nans in array: %s'%np.any(np.isnan(features_in_prf)))
+       
+        wts = pca_wts[:,:,prf_model_index]
+        pre_mean = pca_premean[:,prf_model_index]
+        n_comp_needed = int(pca_ncomp[prf_model_index])
+        
+        # project into pca subspace using saved wts
+        feat_submean = features_in_prf - np.tile(pre_mean[np.newaxis,:], [features_in_prf.shape[0],1])
+        scores = feat_submean @ wts
+        
         actual_max_ncomp = np.max([n_comp_needed, actual_max_ncomp])
         
         scores_each_prf[:,0:n_comp_needed,prf_model_index] = scores[:,0:n_comp_needed]
@@ -173,29 +340,41 @@ def run_pca_each_prf(raw_filename, pca_filename, \
     elapsed = time.time() - t
 
     print('Took %.5f sec to write file'%elapsed)
-      
-  
-        
+
+    
 def run_pca(subject=None, \
             image_set=None, \
             feature_type=None, \
             layer_name = None, \
             which_prf_grid=5, min_pct_var=95,\
+            save_weights=False, \
             max_pc_to_retain=None, debug=False):
        
     if subject is not None:
-        image_set = 'S%d'%subject
-        if subject==999:
-            # 999 is a code for the set of images that are independent of NSD images, 
-            # not shown to any participant.
-            fit_inds = np.ones((10000,),dtype=bool)
-        else:            
-            # training / validation data always split the same way - shared 1000 inds are validation.
-            subject_df = nsd_utils.get_subj_df(subject)
-            fit_inds = np.array(subject_df['shared1000']==False)
+                      
+        if image_set is None:
+            # run PCA on the features for this NSD subject
+            image_set = 'S%d'%subject
+            use_precomputed_weights = False
+            if subject==999:
+                # 999 is a code for the set of images that are independent of NSD images, 
+                # not shown to any participant.
+                fit_inds = np.ones((10000,),dtype=bool)
+            else:            
+                # training / validation data always split the same way - shared 1000 inds are validation.
+                subject_df = nsd_utils.get_subj_df(subject)
+                fit_inds = np.array(subject_df['shared1000']==False)
+        else:
+            # if both subject and image set are specified, this means we should use the weights 
+            # from the NSD subject and apply them to the features from image_set
+            weights_image_set = 'S%d'%subject
+            use_precomputed_weights = True
+            assert(save_weights==False)
     else:
+        # run PCA on features from a non-NSD image set
         assert(image_set is not None)
         subject=0
+        use_precomputed_weights = False
         if image_set=='floc':
             labels_file = os.path.join(default_paths.floc_image_root,'floc_image_labels.csv')
             labels = pd.read_csv(labels_file)
@@ -217,7 +396,19 @@ def run_pca(subject=None, \
                      (image_set, n_ori, n_sf, which_prf_grid))
         pca_filename = os.path.join(path_to_save, '%s_%dori_%dsf_nonlin_PCA_grid%d.h5py'%\
                      (image_set, n_ori, n_sf, which_prf_grid))
-
+                      
+        if save_weights:
+            save_weights_filename = os.path.join(path_to_save, '%s_%dori_%dsf_nonlin_PCA_weights_grid%d.npy'%\
+                     (image_set, n_ori, n_sf, which_prf_grid))
+        else:
+            save_weights_filename = None
+                      
+        if use_precomputed_weights:
+            pca_filename = os.path.join(path_to_save, '%s_%dori_%dsf_nonlin_PCA_wtsfrom%s_grid%d.h5py'%\
+                     (image_set, n_ori, n_sf, weights_image_set, which_prf_grid))
+            load_weights_filename = os.path.join(path_to_save, '%s_%dori_%dsf_nonlin_PCA_weights_grid%d.npy'%\
+                     (weights_image_set, n_ori, n_sf, which_prf_grid))
+                      
         zscore_before_pca = False; zgroup_labels = None;
         prf_batch_size=1500
         
@@ -232,6 +423,18 @@ def run_pca(subject=None, \
         raw_filename = os.path.join(path_to_load, '%s_features_each_prf_grid%d.h5py'%(image_set, which_prf_grid))
         pca_filename = os.path.join(path_to_save, '%s_PCA_grid%d.h5py'%(image_set, which_prf_grid))
         
+        if save_weights:
+            save_weights_filename = os.path.join(path_to_save, '%s_PCA_weights_grid%d.npy'%\
+                     (image_set, which_prf_grid))
+        else:
+            save_weights_filename = None
+                 
+        if use_precomputed_weights:
+            pca_filename = os.path.join(path_to_save, '%s_PCA_wtsfrom%s_grid%d.h5py'%\
+                                        (image_set, weights_image_set, which_prf_grid))
+            load_weights_filename = os.path.join(path_to_save, '%s_PCA_weights_grid%d.npy'%\
+                     (weights_image_set, which_prf_grid))
+            
         zscore_before_pca = True;
         zgroup_labels = np.concatenate([np.zeros(shape=(1,150)), np.ones(shape=(1,1))], axis=1)
         prf_batch_size=1500
@@ -249,7 +452,18 @@ def run_pca(subject=None, \
                                  (image_set, layer_name, which_prf_grid))
         pca_filename = os.path.join(path_to_save, '%s_%s_ReLU_reflect_PCA_grid%d.h5py'%\
                                     (image_set, layer_name, which_prf_grid))
-         
+        if save_weights:
+            save_weights_filename = os.path.join(path_to_save, '%s_%s_ReLU_reflect_PCA_weights_grid%d.npy'%\
+                                    (image_set, layer_name, which_prf_grid))
+        else:
+            save_weights_filename = None
+            
+        if use_precomputed_weights:
+            pca_filename = os.path.join(path_to_save, '%s_%s_ReLU_reflect_PCA_wtsfrom%s_grid%d.h5py'%\
+                                    (image_set, layer_name, weights_image_set, which_prf_grid))
+            load_weights_filename = os.path.join(path_to_save, '%s_%s_ReLU_reflect_PCA_weights_grid%d.npy'%\
+                                    (weights_image_set, layer_name, which_prf_grid))
+            
         zscore_before_pca = True;
         zgroup_labels = None
         prf_batch_size=100
@@ -269,6 +483,18 @@ def run_pca(subject=None, \
                                     for bb in range(n_prf_batches)]
         pca_filename = os.path.join(path_to_save, '%s_%s_%s_PCA_grid%d.h5py'%\
                                     (image_set, model_architecture, layer_name, which_prf_grid))
+        if save_weights:
+            save_weights_filename = os.path.join(path_to_save, '%s_%s_%s_PCA_weights_grid%d.npy'%\
+                                    (image_set, model_architecture, layer_name, which_prf_grid))
+        else:
+            save_weights_filename = None
+            
+        if use_precomputed_weights:
+            pca_filename = os.path.join(path_to_save, '%s_%s_%s_PCA_wtsfrom%s_grid%d.h5py'%\
+                                    (image_set, model_architecture, layer_name, weights_image_set, which_prf_grid))
+            load_weights_filename = os.path.join(path_to_save, '%s_%s_%s_PCA_weights_grid%d.npy'%\
+                                    (weights_image_set, model_architecture, layer_name, which_prf_grid))
+            
         zscore_before_pca = True;
         zgroup_labels = None
         prf_batch_size=100
@@ -279,18 +505,30 @@ def run_pca(subject=None, \
     if not os.path.exists(path_to_save):
         os.mkdir(path_to_save)
         
-    run_pca_each_prf(raw_filename, pca_filename, \
-            fit_inds = fit_inds, \
-            prf_batch_size=prf_batch_size, 
-            zscore_before_pca = zscore_before_pca, \
-            zgroup_labels = zgroup_labels,\
-            min_pct_var=min_pct_var,\
-            max_pc_to_retain=max_pc_to_retain,\
-            save_dtype=np.float32, compress=True, \
-            debug=debug)
-    
+    if use_precomputed_weights:
+        apply_pca_each_prf(raw_filename, pca_filename, \
+                load_weights_filename, \
+                prf_batch_size=prf_batch_size, 
+                zscore_before_pca = zscore_before_pca, \
+                zgroup_labels = zgroup_labels,\
+                save_dtype=np.float32, compress=True, \
+                debug=debug)
 
-def do_pca(values, max_pc_to_retain=None):
+    else:
+        run_pca_each_prf(raw_filename, pca_filename, \
+                fit_inds = fit_inds, \
+                prf_batch_size=prf_batch_size, 
+                zscore_before_pca = zscore_before_pca, \
+                zgroup_labels = zgroup_labels,\
+                min_pct_var=min_pct_var,\
+                max_pc_to_retain=max_pc_to_retain,\
+                save_weights = save_weights, \
+                save_weights_filename = save_weights_filename, \
+                save_dtype=np.float32, compress=True, \
+                debug=debug)
+
+        
+def compute_pca(values, max_pc_to_retain=None):
     """
     Apply PCA to the data, return reduced dim data as well as weights, var explained.
     """
@@ -331,6 +569,9 @@ if __name__ == '__main__':
     parser.add_argument("--debug", type=int,default=0,
                     help="want to run a fast test version of this script to debug? 1 for yes, 0 for no")
 
+    parser.add_argument("--save_weights", type=int,default=0,
+                    help="want to save the weights to reproduce the pca later? 1 for yes, 0 for no")
+
     parser.add_argument("--max_pc_to_retain", type=int,default=0,
                     help="max pc to retain? enter 0 for None")
     parser.add_argument("--min_pct_var", type=int,default=95,
@@ -358,7 +599,8 @@ if __name__ == '__main__':
                             layer_name = layer,
                             which_prf_grid=args.which_prf_grid,
                             min_pct_var=args.min_pct_var, 
-                            max_pc_to_retain=args.max_pc_to_retain, 
+                            max_pc_to_retain=args.max_pc_to_retain,
+                            save_weights = args.save_weights, 
                             debug=args.debug==1)
     elif args.type=='clip':
         layers = ['block%d'%(ll) for ll in range(16)]
@@ -370,6 +612,7 @@ if __name__ == '__main__':
                             which_prf_grid=args.which_prf_grid,
                             min_pct_var=args.min_pct_var, 
                             max_pc_to_retain=args.max_pc_to_retain, 
+                            save_weights = args.save_weights, 
                             debug=args.debug==1)
     else:
         run_pca(subject=args.subject, 
@@ -378,5 +621,6 @@ if __name__ == '__main__':
                             which_prf_grid=args.which_prf_grid,
                             min_pct_var=args.min_pct_var, 
                             max_pc_to_retain=args.max_pc_to_retain, 
+                            save_weights = args.save_weights, 
                             debug=args.debug==1)
     

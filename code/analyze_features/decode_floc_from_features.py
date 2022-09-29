ffbe19a14a54cd7dad07f0996d1ee900eadf7360
Margaret Marie Henderson <mmhender@cmu.edu>
2022-09-29 19:20:34 -0400

    code to run decoding of floc categories from each feature space

---
+++ code/analyze_features/decode_floc_from_features.py
@@ -0,0 +1,130 @@
+import sys, os
+import numpy as np
+import time, h5py
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import argparse
+import pandas as pd   
+
+from utils import default_paths, stats_utils, floc_utils
+from model_fitting import initialize_fitting 
+from feature_extraction import default_feature_loaders_visvssem
+
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+def run_decoding(image_set='floc', 
+                 feature_type='gabor_solo', \
+                 which_prf_grid=1, debug=False, \
+                 layer_name=None):
+      
+    models = initialize_fitting.get_prf_models(which_grid = which_prf_grid)    
+    n_prfs = len(models)
+    print('feature_type=%s, which_prf_grid=%d'%(feature_type, which_prf_grid))
+    print('debug=%s'%debug)
+    sys.stdout.flush()
+
+    image_inds_use = floc_utils.load_balanced_floc_set()
+    
+    labels_file = os.path.join(default_paths.floc_image_root,'floc_image_labels.csv')
+    labels = pd.read_csv(labels_file)
+
+    domain_labels = np.array([labels['domain']==domain for domain in floc_utils.domains]).T.astype(int)
+    domain_labels = domain_labels[image_inds_use,:]
+    
+    discrim_type_list = ['%s > other domains'%domain for domain in floc_utils.domains]
+
+    print('Number of images using: %d'%domain_labels.shape[0])
+    print('doing decoding for:')
+    print(discrim_type_list)
+    sys.stdout.flush()
+
+    n_trials = domain_labels.shape[0]
+    n_sem_axes = domain_labels.shape[1] 
+    
+    # create feature loaders
+    feat_loader, path_to_load = \
+        default_feature_loaders_visvssem.get_feature_loaders(image_set, feature_type, which_prf_grid)
+    
+    # Choose where to save results
+    if debug:
+        path_to_save = os.path.join(path_to_load, 'feature_decoding_floc_DEBUG')
+    else:
+        path_to_save = os.path.join(path_to_load, 'feature_decoding_floc')
+    if not os.path.exists(path_to_save):
+        os.mkdir(path_to_save)
+
+    fn2save = os.path.join(path_to_save, \
+               '%s_%s_LDA_all_grid%d.npy'%(image_set, feature_type, which_prf_grid))
+       
+    print(fn2save)
+    sys.stdout.flush()
+    
+    acc_each_prf = np.zeros((n_prfs, n_sem_axes), dtype=np.float32)
+    dprime_each_prf = np.zeros((n_prfs, n_sem_axes), dtype=np.float32)
+    
+    # first looping over pRFs
+    for prf_model_index in range(n_prfs):
+
+        if debug and prf_model_index>1:
+            continue
+
+        print('\nProcessing pRF %d of %d'%(prf_model_index, n_prfs))
+
+        image_inds = np.where(image_inds_use)[0]
+        print(len(image_inds))
+        features_in_prf, _ = feat_loader.load(image_inds, prf_model_index)
+       
+        print('Size of features array for this image set and prf is:')
+        print(features_in_prf.shape)
+        assert(not np.any(np.isnan(features_in_prf)))
+        # assert(not np.any(np.sum(features_in_prf, axis=0)==0))
+
+        # then looping over axes to decode
+        for aa in range(n_sem_axes):
+
+            if prf_model_index==0:
+                print('processing axis: %s'%discrim_type_list[aa])
+                print('labels: ')
+                print(np.unique(domain_labels[:,aa]))
+            
+            X = features_in_prf
+            y = domain_labels[:,aa]
+            tst_acc, tst_dprime = stats_utils.decode_lda(X, y, n_crossval_folds=10, debug=debug)
+            
+            acc_each_prf[prf_model_index,aa] = tst_acc
+            dprime_each_prf[prf_model_index, aa] = tst_dprime
+            
+            # print to see how we are doing so far
+            print('decode %s, prf %d: acc=%.2f, dprime=%.2f'%(discrim_type_list[aa], prf_model_index, tst_acc, tst_dprime))
+            sys.stdout.flush()
+
+
+    print('saving to %s'%fn2save)
+    np.save(fn2save, {'acc': acc_each_prf, \
+                      'dprime': dprime_each_prf, 
+                      'discrim_type_list': discrim_type_list})
+
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--image_set", type=str,default='floc',
+                    help="which image set?")
+    parser.add_argument("--feature_type", type=str,default='sketch_tokens',
+                    help="what kind of features are we using?")
+    parser.add_argument("--debug", type=int,default=0,
+                    help="want to run a fast test version of this script to debug? 1 for yes, 0 for no")
+    parser.add_argument("--which_prf_grid", type=int,default=1,
+                    help="which prf grid to use")
+    
+    args = parser.parse_args()
+
+    if args.debug:
+        print('DEBUG MODE\n')
+        
+    sys.stdout.flush()
+     
+    run_decoding(image_set=args.image_set, feature_type=args.feature_type, debug=args.debug==1, \
+                  which_prf_grid=args.which_prf_grid)
+    
---
+++ code/feature_extraction/default_feature_loaders_visvssem.py
@@ -0,0 +1,88 @@
+import numpy as np
+import time, h5py
+from utils import default_paths
+from feature_extraction import fwrf_features, merge_features
+
+def get_feature_loaders(image_set, feature_type, which_prf_grid=5):
+
+    """
+    Code to quickly create feature loader objects (from fwrf_features.fwrf_feature_loader)
+    with some default params
+    """
+
+    if feature_type=='gabor_solo':
+        path_to_load = default_paths.gabor_texture_feat_path
+        feat_loaders = fwrf_features.fwrf_feature_loader(image_set=image_set,\
+                                                         use_noavg=True, 
+                                                         pca_subject=1, 
+                                                         which_prf_grid=which_prf_grid,\
+                                                         feature_type='gabor_solo', \
+                                                         n_ori=12, n_sf=8, nonlin=True)
+    elif 'pyramid_texture' in feature_type:
+        path_to_load = default_paths.pyramid_texture_feat_path
+        feat_loaders = fwrf_features.fwrf_feature_loader(image_set=image_set, 
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='pyramid_texture',\
+                                                        n_ori=4, n_sf=4,\
+                                                        pca_subject=1, 
+                                                        pca_type='pcaHL')     
+    elif 'color' in feature_type:
+        path_to_load = default_paths.color_feat_path
+        feat_loaders = fwrf_features.fwrf_feature_loader(image_set=image_set,
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='color',\
+                                                        pca_subject=1, 
+                                                        use_noavg=True)       
+ 
+    elif 'sketch_tokens' in feature_type:
+        path_to_load = default_paths.sketch_token_feat_path
+        feat_loaders = fwrf_features.fwrf_feature_loader(image_set=image_set,
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='sketch_tokens',\
+                                                        use_pca_feats = False, \
+                                                        use_noavg=False,
+                                                        use_residual_st_feats=False)
+
+    elif 'alexnet' in feature_type:
+        path_to_load = default_paths.alexnet_feat_path
+        blurface = 'blurface' in feature_type
+        floaders = []
+        layer_names=['Conv%d_ReLU'%ll for ll in np.arange(1,6)]
+        for layer_name in layer_names:
+              
+            f = fwrf_features.fwrf_feature_loader(image_set=image_set,
+                                                which_prf_grid=which_prf_grid, \
+                                                feature_type='alexnet',layer_name=layer_name,\
+                                                padding_mode = 'reflect', \
+                                                pca_subject=1, 
+                                                use_noavg=False, blurface=blurface)
+            floaders.append(f)
+        feat_loaders = merge_features.combined_feature_loader(floaders, layer_names)
+        
+
+    elif 'clip' in feature_type or 'resnet' in feature_type:
+        path_to_load = default_paths.clip_feat_path
+        if 'clip' in feature_type:
+            training_type='clip'
+        elif 'blurface' in feature_type:
+            training_type='blurface'
+        else:
+            training_type='imgnet'
+        floaders = []
+        layer_names=['block%d'%ll for ll in [2,6,12,15]]
+        for layer_name in layer_names:
+        
+            f = fwrf_features.fwrf_feature_loader(image_set=image_set,
+                                                        which_prf_grid=which_prf_grid, \
+                                                        feature_type='resnet',layer_name=layer_name,\
+                                                        model_architecture='RN50',
+                                                        use_noavg=True,
+                                                        pca_subject=1, 
+                                                        training_type=training_type)
+            floaders.append(f)
+        feat_loaders = merge_features.combined_feature_loader(floaders, layer_names)
+        
+    else:
+        raise RuntimeError('feature type %s not recognized'%feature_type)
+    
+    return feat_loaders, path_to_load

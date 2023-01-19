444c010b0e5a7b950cf578c00c033a40dd44032d
Margaret Marie Henderson <mmhender@cmu.edu>
2023-01-18 23:00:38 -0500

    add a new function

--- code/utils/numpy_utils.py
+++ code/utils/numpy_utils.py
@@ -198,6 +198,57 @@ def bin_ydata_by_xdata(xdata, ydata, n_bins, linear_bins=True, remove_nans=True,
         
     return to_return
 
+def bin_sums_ydata_by_xdata(xdata, ydata, n_bins, linear_bins=True, remove_nans=True, \
+                       return_edges=False, use_unique=False):
+           
+    if len(xdata.shape)>1:
+        xdata = np.squeeze(xdata)
+    if len(ydata.shape)>1:
+        ydata = np.squeeze(ydata)
+    assert((len(xdata.shape)==1) and (len(ydata.shape)==1))
+    
+    if linear_bins:
+        # break x axis into linearly spaced increments
+        bin_edges = np.linspace(np.min(xdata), np.max(xdata)+0.0001, n_bins+1)
+        bin_centers = bin_edges[0:-1]+(bin_edges[1]-bin_edges[0])/2
+    elif use_unique:
+        # force the bin centers to take on actual values in the data.
+        bin_centers = np.unique(xdata)
+        bin_edges = np.concatenate([bin_centers-0.01, [bin_centers[-1]+0.01]], axis=0)
+        n_bins=len(bin_centers)
+    else:
+        # bin according to data density
+        bin_edges = np.quantile(xdata, np.linspace(0,1,n_bins+1))
+        bin_edges[-1]+=0.0001
+        bin_centers = bin_edges[0:-1]+np.diff(bin_edges)/2
+
+    xbinned = np.zeros((n_bins,))
+    ybinned = np.zeros((n_bins,))
+    used_yet = np.zeros((len(xdata),))
+    for bb in range(n_bins):
+        inds = (xdata>=bin_edges[bb]) & (xdata<bin_edges[bb+1])
+        xbinned[bb] = bin_centers[bb]
+        if np.sum(inds)>0:
+            used_yet[inds] += 1
+            ybinned[bb] = np.sum(ydata[inds])
+        else:
+            ybinned[bb] = np.nan
+            
+    assert(np.all(used_yet)==1)
+   
+    if remove_nans:
+        good = ~np.isnan(ybinned)
+        xbinned=xbinned[good] 
+        bin_edges=np.concatenate([bin_edges[0:-1][good], [bin_edges[-1]]], axis=0)
+        ybinned=ybinned[good]
+        
+    to_return = xbinned, ybinned
+    
+    if return_edges:
+        to_return += bin_edges,
+        
+    return to_return
+
 
 def list_all_combs(values, n_levels):
     
